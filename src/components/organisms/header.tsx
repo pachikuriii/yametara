047b1892c24a35facca1aa549b7bae518fa62420
pachikuriii <stakahashi0728@gmail.com>
2023-01-10 18:52:13 +0900

    ヘッダーコンポーネントを作成

---
+++ src/components/organisms/header.tsx
@@ -0,0 +1,9 @@
+import Image from 'next/image';
+import styles from './Header.module.css';
+export default function Header() {
+  return (
+    <header className={styles.box}>
+      <Image src='/yametara-logo.svg' alt='logo' width='150' height='100' />
+    </header>
+  );
+}
