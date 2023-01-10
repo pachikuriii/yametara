b4f342ba8377ffd5b6be38b1e7c2f9c2fdc4b730
pachikuriii <stakahashi0728@gmail.com>
2023-01-10 11:20:38 +0900

    title,descriptionの記述を変更

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,6 +5,11 @@ export default function Document() {
     <Html lang='ja'>
       <Head>
         <link rel='icon' href='/favicon.ico' />
+        <meta
+          name='description'
+          content='yametaraは会社を辞めた後、すぐに就職しない選択をはじめてする方におすすめの退職後の手続きシミュレーターです。いくつかの質問に答えるだけで会社を辞めた後に必要な手続きを一括で把握することができます。'
+        />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
       </Head>
       <body>
         <Main />
