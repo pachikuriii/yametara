98cb56e40f67b583070fed1248daae39d739e8ad
pachikuriii <stakahashi0728@gmail.com>
2023-01-10 22:40:51 +0900

    質問ページに表示させるためのヘッダーを作成

--- src/components/organisms/header.tsx
+++ src/components/organisms/header.tsx
@@ -4,6 +4,7 @@ export default function Header() {
   return (
     <header className={styles.box}>
       <Image src='/yametara-logo.svg' alt='logo' width='150' height='100' />
+      <p>ページごとの質問</p>
     </header>
   );
 }
