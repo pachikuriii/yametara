194d5f97615c6da487c9f1e30032164e963a7b4f
pachikuriii <stakahashi0728@gmail.com>
2023-01-10 19:02:10 +0900

    cssのカスタマイズを追加

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,22 @@ module.exports = {
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
+    boxShadow: {
+      md: '0 9px 0px rgba(89, 197, 139, 1)',
+      none: 'none',
+    },
     extend: {},
   },
   plugins: [require('daisyui')],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: '#E5F8F4',
+          secondary: '#59C58B',
+          accent: '#FF354B',
+        },
+      },
+    ],
+  },
 };
