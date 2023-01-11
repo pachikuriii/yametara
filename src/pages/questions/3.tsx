49239921d687700252268b91c731d50d069935e5
pachikuriii <stakahashi0728@gmail.com>
2023-01-11 22:01:08 +0900

    q3の中身を作成

--- src/pages/questions/3.tsx
+++ src/pages/questions/3.tsx
@@ -1,11 +1,16 @@
 import { motion } from 'framer-motion';
 import Head from 'next/head';
+import { Navigation } from 'swiper';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import AnswerSelectButton from '../../components/atoms/answer-select-button';
 import Card from '../../components/atoms/card';
 import ButtonsPager from '../../components/molecules/buttons-pager';
 import Footer from '../../components/organisms/question/footer';
 import Header from '../../components/organisms/question/header';
 import QuestionTemplate from '../../components/templates/questions/question';
 import styles from '../../styles/Question.module.css';
+import 'swiper/css';
+import 'swiper/css/navigation';
 export default function Home() {
   return (
     <>
@@ -23,7 +28,45 @@ export default function Home() {
             transition={{ duration: 0.3 }}
           >
             <Card>
-              <p>らら</p>
+              <h2 className='card-title'>
+                退職予定日におけるあなたの年齢とお住まいの住所の郵便番号を教えてください
+              </h2>
+              <div>
+                <p>年齢</p>
+                <div>
+                  <Swiper
+                    slidesPerView={3}
+                    spaceBetween={100}
+                    className='mySwiper'
+                    navigation={true}
+                    modules={[Navigation]}
+                  >
+                    <SwiperSlide>
+                      <AnswerSelectButton>20歳</AnswerSelectButton>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <AnswerSelectButton>30歳</AnswerSelectButton>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <AnswerSelectButton>40歳</AnswerSelectButton>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <AnswerSelectButton>50歳</AnswerSelectButton>
+                    </SwiperSlide>
+                    <SwiperSlide>
+                      <AnswerSelectButton>60歳</AnswerSelectButton>
+                    </SwiperSlide>
+                  </Swiper>
+                </div>
+              </div>
+              <div>
+                <p>郵便番号</p>
+                <input
+                  type='text'
+                  placeholder='予定日を入力してください'
+                  className='input w-full max-w-xs border-accent'
+                />
+              </div>
             </Card>
           </motion.div>
           <ButtonsPager></ButtonsPager>
