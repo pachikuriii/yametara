df18c94744bd792e373f68e5e61c7b3b54b1965a
pachikuriii <stakahashi0728@gmail.com>
2023-01-11 16:04:35 +0900

    indexページのコンポーネントを整理

---
+++ src/components/organisms/index/main.tsx
@@ -0,0 +1,22 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import Button from '../../atoms/button';
+import styles from './Main.module.css';
+
+export default function IndexMain() {
+  return (
+    <div className={styles.box}>
+      <Image src='/yametara-logo.svg' alt='logo' width='500' height='100' />
+      <div>
+        <p>
+          いくつかの質問に答えるだけで
+          会社を辞めて、すぐに就職する予定がない場合に必要な
+          社会保険や税金の手続き内容を 簡単にシミュレーションできます。
+        </p>
+      </div>
+      <Link href='/questions/1'>
+        <Button>はじめる</Button>
+      </Link>
+    </div>
+  );
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,7 @@
 import { motion } from 'framer-motion';
 import Head from 'next/head';
-import Image from 'next/image';
-import Link from 'next/link';
-import Button from '../components/atoms/button';
-import Footer from '../components/organisms/index-footer';
+import Footer from '../components/organisms/index/footer';
+import Main from '../components/organisms/index/main';
 import styles from '../styles/Home.module.css';
 export default function Home() {
   return (
@@ -21,24 +19,7 @@ export default function Home() {
         transition={{ duration: 0.3 }}
       >
         <main className={styles.main}>
-          <div>
-            <Image
-              src='/yametara-logo.svg'
-              alt='logo'
-              width='500'
-              height='100'
-            />
-            <div>
-              <p>
-                いくつかの質問に答えるだけで
-                会社を辞めて、すぐに就職する予定がない場合に必要な
-                社会保険や税金の手続き内容を 簡単にシミュレーションできます。
-              </p>
-            </div>
-            <Link href='/questions/1'>
-              <Button>はじめる</Button>
-            </Link>
-          </div>
+          <Main></Main>
           <Footer></Footer>
         </main>
       </motion.div>
