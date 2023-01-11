f4490d260b1e172bfcebd27b08b1065a24fef894
pachikuriii <stakahashi0728@gmail.com>
2023-01-11 16:19:26 +0900

    ボタンコンポーネントをリファクタリング

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -1,9 +1,10 @@
-import { FC, ReactNode } from 'react';
+import { ReactNode } from 'react';
 interface Props {
   onClick?: () => void;
   children: ReactNode;
 }
-const Button: FC<Props> = ({ children, onClick }) => {
+
+const Button = ({ onClick, children }: Props) => {
   return (
     <button
       className='btn btn-outline text-accent bg-primary rounded-full border-secondary no-animation hover:bg-secondary-focus shadow-md'
