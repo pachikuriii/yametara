220b53b89076e96285c619ff75152b1b65109c96
pachikuriii <stakahashi0728@gmail.com>
2023-01-11 16:26:27 +0900

    質問ページのヘッダーコンポーネントを子要素で表示切り替えできるように

--- src/components/organisms/question/header.tsx
+++ src/components/organisms/question/header.tsx
@@ -1,10 +1,18 @@
 import Image from 'next/image';
+import { ReactNode } from 'react';
 import styles from './Header.module.css';
-export default function Header() {
+
+interface Props {
+  children: ReactNode;
+}
+
+const Header = ({ children }: Props) => {
   return (
     <header className={styles.box}>
       <Image src='/yametara-logo.svg' alt='logo' width='150' height='100' />
-      <p>ページごとの質問</p>
+      <p>{children}</p>
     </header>
   );
-}
+};
+
+export default Header;
