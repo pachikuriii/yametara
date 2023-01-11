78f70d8109d0c9f743d8259f64ec9dc7c143d35c
pachikuriii <stakahashi0728@gmail.com>
2023-01-11 23:34:07 +0900

    q8の中身を作成

--- src/pages/questions/8.tsx
+++ src/pages/questions/8.tsx
@@ -26,7 +26,21 @@ export default function Home() {
               transition={{ duration: 0.3 }}
             >
               <Card>
-                <p>らら</p>
+                <h2 className='card-title'>
+                  今年度の住民税について、残りの支払いはどうしますか？
+                </h2>
+
+                <input type='radio' name='radio-1' className='radio' checked />
+                <p>
+                  退職時に給与/退職金から会社に翌年5月分まで天引きしてもらう（一括徴収）
+                </p>
+
+                <input type='radio' name='radio-1' className='radio' />
+                <p>
+                  送付される納税通知書に基づいて自分で分割で納める（普通徴収）
+                </p>
+                <input type='radio' name='radio-1' className='radio' />
+                <p>今年度の住民税の支払いはなし</p>
               </Card>
             </motion.div>
 
