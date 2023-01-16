eb9a003cb9f068994eb83c8dc4b9a888e8ba2b4e
pachikuriii <stakahashi0728@gmail.com>
2023-01-16 13:52:09 +0900

    q8の回答をLocal Strageへ保存できるように

--- src/pages/questions/8.tsx
+++ src/pages/questions/8.tsx
@@ -1,13 +1,26 @@
 import { motion } from 'framer-motion';
 import Head from 'next/head';
 import Link from 'next/link';
+import { useState, useEffect } from 'react';
 import Button from '../../components/atoms/button';
 import Card from '../../components/atoms/card';
 import Footer from '../../components/organisms/question/footer';
 import Header from '../../components/organisms/question/header';
 import QuestionTemplate from '../../components/templates/questions/question';
+import LocalStorage from '../../local-stroage';
 import styles from '../../styles/Question.module.css';
 export default function Home() {
+  const [tax, setTax] = useState(0);
+
+  function reflectDataToLocalStrage() {
+    const localStrage = LocalStorage.fetch();
+    localStrage.tax = tax;
+    LocalStorage.save(localStrage);
+  }
+
+  useEffect(() => {
+    reflectDataToLocalStrage();
+  });
   return (
     <>
       <Head>
@@ -30,17 +43,41 @@ export default function Home() {
                   今年度の住民税について、残りの支払いはどうしますか？
                 </h2>
 
-                <input type='radio' name='radio-1' className='radio' checked />
-                <p>
-                  退職時に給与/退職金から会社に翌年5月分まで天引きしてもらう（一括徴収）
-                </p>
+                <div
+                  onClick={() => {
+                    setTax(1);
+                  }}
+                >
+                  <input
+                    type='radio'
+                    name='radio-1'
+                    className='radio'
+                    checked
+                  />
+                  <p>
+                    退職時に給与/退職金から会社に翌年5月分まで天引きしてもらう（一括徴収）
+                  </p>
+                </div>
 
-                <input type='radio' name='radio-1' className='radio' />
-                <p>
-                  送付される納税通知書に基づいて自分で分割で納める（普通徴収）
-                </p>
-                <input type='radio' name='radio-1' className='radio' />
-                <p>今年度の住民税の支払いはなし</p>
+                <div
+                  onClick={() => {
+                    setTax(2);
+                  }}
+                >
+                  <input type='radio' name='radio-1' className='radio' />
+                  <p>
+                    送付される納税通知書に基づいて自分で分割で納める（普通徴収）
+                  </p>
+                </div>
+
+                <div
+                  onClick={() => {
+                    setTax(3);
+                  }}
+                >
+                  <input type='radio' name='radio-1' className='radio' />
+                  <p>今年度の住民税の支払いはなし</p>
+                </div>
               </Card>
             </motion.div>
 
