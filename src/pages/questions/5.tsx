7b7ce72b2bef4d5a66e31b52a6da42e57a7280aa
pachikuriii <stakahashi0728@gmail.com>
2023-01-16 13:22:26 +0900

    q5の回答をLocal Strageへ保存できるように

--- src/pages/questions/5.tsx
+++ src/pages/questions/5.tsx
@@ -1,5 +1,6 @@
 import { motion } from 'framer-motion';
 import Head from 'next/head';
+import { useState, useEffect } from 'react';
 import { Navigation } from 'swiper';
 import { Swiper, SwiperSlide } from 'swiper/react';
 import AnswerSelectButton from '../../components/atoms/answer-select-button';
@@ -8,10 +9,26 @@ import ButtonsPager from '../../components/molecules/buttons-pager';
 import Footer from '../../components/organisms/question/footer';
 import Header from '../../components/organisms/question/header';
 import QuestionTemplate from '../../components/templates/questions/question';
+import LocalStorage from '../../local-stroage';
 import styles from '../../styles/Question.module.css';
 import 'swiper/css';
 import 'swiper/css/navigation';
 export default function Home() {
+  const [employmentInsuranceLastTwoYears, setemploymentInsuranceLastTwoYears] =
+    useState(0);
+  const [employmentInsuranceInThePast, setemploymentInsuranceInThePast] =
+    useState(0);
+
+  function reflectDataToLocalStrage() {
+    const localStrage = LocalStorage.fetch();
+    localStrage.emp_ins_last_two_years = employmentInsuranceLastTwoYears;
+    localStrage.emp_ins_total = employmentInsuranceInThePast;
+    LocalStorage.save(localStrage);
+  }
+
+  useEffect(() => {
+    reflectDataToLocalStrage();
+  });
   return (
     <>
       <Head>
@@ -35,9 +52,27 @@ export default function Home() {
               <p>雇用保険の被保険者期間の数え方について</p>
               <div>
                 <p>退職予定日までの2年間では…</p>
-                <AnswerSelectButton>6ヶ月未満</AnswerSelectButton>
-                <AnswerSelectButton>6ヶ月以上1年未満</AnswerSelectButton>
-                <AnswerSelectButton>1年以上</AnswerSelectButton>
+                <AnswerSelectButton
+                  onClick={() => {
+                    setemploymentInsuranceLastTwoYears(1);
+                  }}
+                >
+                  6ヶ月未満
+                </AnswerSelectButton>
+                <AnswerSelectButton
+                  onClick={() => {
+                    setemploymentInsuranceLastTwoYears(2);
+                  }}
+                >
+                  6ヶ月以上1年未満
+                </AnswerSelectButton>
+                <AnswerSelectButton
+                  onClick={() => {
+                    setemploymentInsuranceLastTwoYears(3);
+                  }}
+                >
+                  1年以上
+                </AnswerSelectButton>
               </div>
               <div>
                 <p>退職予定日までの通算では…</p>
@@ -47,18 +82,52 @@ export default function Home() {
                   className='mySwiper'
                   navigation={true}
                   modules={[Navigation]}
+                  centeredSlides={true}
                 >
                   <SwiperSlide>
-                    <AnswerSelectButton>1年未満</AnswerSelectButton>
+                    <AnswerSelectButton
+                      onClick={() => {
+                        setemploymentInsuranceInThePast(1);
+                      }}
+                    >
+                      1年未満
+                    </AnswerSelectButton>
                   </SwiperSlide>
                   <SwiperSlide>
-                    <AnswerSelectButton>1年以上5年未満</AnswerSelectButton>
+                    <AnswerSelectButton
+                      onClick={() => {
+                        setemploymentInsuranceInThePast(2);
+                      }}
+                    >
+                      1年以上5年未満
+                    </AnswerSelectButton>
                   </SwiperSlide>
                   <SwiperSlide>
-                    <AnswerSelectButton>5年以上10年未満</AnswerSelectButton>
+                    <AnswerSelectButton
+                      onClick={() => {
+                        setemploymentInsuranceInThePast(3);
+                      }}
+                    >
+                      5年以上10年未満
+                    </AnswerSelectButton>
                   </SwiperSlide>
                   <SwiperSlide>
-                    <AnswerSelectButton>10年以上</AnswerSelectButton>
+                    <AnswerSelectButton
+                      onClick={() => {
+                        setemploymentInsuranceInThePast(4);
+                      }}
+                    >
+                      10年以上20年未満
+                    </AnswerSelectButton>
+                  </SwiperSlide>
+                  <SwiperSlide>
+                    <AnswerSelectButton
+                      onClick={() => {
+                        setemploymentInsuranceInThePast(5);
+                      }}
+                    >
+                      20年以上
+                    </AnswerSelectButton>
                   </SwiperSlide>
                 </Swiper>
               </div>
