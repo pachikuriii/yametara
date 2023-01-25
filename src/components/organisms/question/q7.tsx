44b5452c2bf9335a517e60153da5a0f44eb92994
pachikuriii <stakahashi0728@gmail.com>
2023-01-25 17:30:30 +0900

    タブのコンポーネントを削除

--- src/components/organisms/question/q7.tsx
+++ src/components/organisms/question/q7.tsx
@@ -1,11 +1,11 @@
 import { useState, useEffect } from 'react';
 import Button from '../../atoms/button';
-import Tabs from '../../molecules/question/q7/tabs';
+
 interface Props {
   sethealthInsuranceAfterRetirement: (number: number) => void;
 }
 const Q7 = (props: Props) => {
-  const [openTab, setOpenTab] = useState(1);
+  const [tab, setTab] = useState(1);
   const [selectedTab, setSelectedTab] = useState(1);
   useEffect(() => {
     props.sethealthInsuranceAfterRetirement(selectedTab);
@@ -13,13 +13,52 @@ const Q7 = (props: Props) => {
   return (
     <>
       <div className='flex flex-wrap'>
-        <div className=' tabs tabs-boxed bg-primary'>
-          <Tabs openTab={openTab} setOpenTab={setOpenTab}></Tabs>
+        <div className='tabs tabs-boxed bg-primary'>
+          <div>
+            <a
+              className={
+                'tab ' +
+                (tab === 1 ? 'bg-accent rounded-full text-primary' : '')
+              }
+              onClick={(event) => {
+                event.preventDefault();
+                setTab(1);
+              }}
+            >
+              任意継続健康保険
+            </a>
+
+            <a
+              className={
+                'tab ' +
+                (tab === 2 ? 'bg-accent rounded-full text-primary' : '')
+              }
+              onClick={(event) => {
+                event.preventDefault();
+                setTab(2);
+              }}
+            >
+              国民健康保険
+            </a>
+
+            <a
+              className={
+                'tab ' +
+                (tab === 3 ? 'bg-accent rounded-full text-primary' : '')
+              }
+              onClick={(event) => {
+                event.preventDefault();
+                setTab(3);
+              }}
+            >
+              家族の健康保険（被扶養者）
+            </a>
+          </div>
 
           <div className='relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded'>
             <div className='px-4 py-5 flex-auto'>
               <div className='tab-content tab-space'>
-                <div className={openTab === 1 ? 'block' : 'hidden'}>
+                <div className={tab === 1 ? 'block' : 'hidden'}>
                   <div>
                     <p className='border-b-4 border-dotted w-fit'>
                       国民健康保険への加入
@@ -33,20 +72,20 @@ const Q7 = (props: Props) => {
                     この保険への加入を検討する
                   </Button>
                 </div>
-                <div className={openTab === 2 ? 'block' : 'hidden'}>
+                <div className={tab === 2 ? 'block' : 'hidden'}>
                   <p className='text-black'>年金</p>
                   <Button onClick={() => setSelectedTab(2)}>
                     この保険への加入を検討する
                   </Button>
                 </div>
-                <div className={openTab === 3 ? 'block' : 'hidden'}>
+                <div className={tab === 3 ? 'block' : 'hidden'}>
                   <p className='text-black'>雇用保険</p>
                   <Button onClick={() => setSelectedTab(3)}>
                     この保険への加入を検討する
                   </Button>
                 </div>
 
-                <div className={openTab === 4 ? 'block' : 'hidden'}>
+                <div className={tab === 4 ? 'block' : 'hidden'}>
                   <p className='text-black'>税金</p>
                 </div>
               </div>
