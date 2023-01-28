9cb61fa3bd5c9165592c39a4eafe8ed8914f6581
pachikuriii <stakahashi0728@gmail.com>
2023-01-28 21:24:00 +0900

    結果ページの年金に退職日と手続き期限を紐づけた

--- src/components/organisms/result/detail.tsx
+++ src/components/organisms/result/detail.tsx
@@ -1,5 +1,14 @@
 import HealthlInsurance from './health-insurance';
-
+import Pension from './pension';
 export default function Detail() {
-  return <>{<HealthlInsurance></HealthlInsurance>}</>;
+  return (
+    <>
+      {
+        <div>
+          <HealthlInsurance></HealthlInsurance>
+          <Pension></Pension>
+        </div>
+      }
+    </>
+  );
 }
---
+++ src/components/organisms/result/pension.tsx
@@ -0,0 +1,66 @@
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import dayjs from '../../../day-js';
+import { retirementDateState } from '../../../local-stroage';
+import CheckedTodoPlate from '../../molecules/checked-todo-plate';
+
+export default function Pension() {
+  const [storedRetirementDate] = useRecoilState(retirementDateState);
+  const [DayAfterRetirementDate, setDayAfterRetirementDate] = useState('');
+  const [pensionApplyDeadline, setPensionApplyDeadline] = useState('');
+
+  useEffect(() => {
+    setDayAfterRetirementDate(
+      dayjs(storedRetirementDate).add(1, 'day').format('M月D日'),
+    );
+    setPensionApplyDeadline(
+      dayjs(storedRetirementDate).add(15, 'day').format('M月D日'),
+    );
+  }, [storedRetirementDate]);
+
+  return (
+    <div>
+      <CheckedTodoPlate>年金</CheckedTodoPlate>
+      <div>
+        <h4 className='text-lg border-b-4 border-dotted w-fit'>
+          国民年金への加入手続き
+        </h4>
+        <p>どこで？：住所地の市区役所/町村役場の窓口やマイナポータルなどで</p>
+
+        <p>
+          いつまでに？退職日の翌日{DayAfterRetirementDate}から
+          {pensionApplyDeadline}までに
+        </p>
+        <p>持ち物</p>
+      </div>
+    </div>
+  );
+}
+
+//           <CheckedTodoPlate>雇用保険</CheckedTodoPlate>
+//           <p className='text-lg border-b-4 border-dotted w-fit'>
+//             雇用保険の失業給付の受給手続き
+//           </p>
+//           <p>
+//             あなたは●日分のの失業給付（基本手当）の受給資格があります。基本手当の受給のためにハローワーク●●で手続きが必要です。
+//           </p>
+//         </div>
+
+//         <div>
+//           <CheckedTodoPlate>税金</CheckedTodoPlate>
+//           <div>
+//             <p className='text-lg border-b-4 border-dotted w-fit'>住民税</p>
+//             <p>
+//               職時に一括徴収しない場合は自治体から送られてくる納付書で支払いが必要です。
+//               退職時に一括徴収しているため今年度の支払いはありません。
+//             </p>
+//           </div>
+//           <div>
+//             <p className='text-lg border-b-4 border-dotted w-fit'>所得税</p>
+//             <p>
+//               年の途中で退職し、年内に再就職しない場合は年末調整を受けられません。そのため所得税を納め過ぎになる場合があります。この納め過ぎの所得税は、翌年になってから確定申告をすることで還付を受けられます。
+//               確定申告するには●月●日〜●月●日までの間に管轄の税務署で手続きが必要です。
+//             </p>
+//           </div>
+//         </div>
+//       </div>
