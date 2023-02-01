f956434d9ba7bf5cb33a305975497c2bd8a4ba11
pachikuriii <stakahashi0728@gmail.com>
2023-02-02 00:41:51 +0900

    エラーへ対応

--- src/components/organisms/question/q7.tsx
+++ src/components/organisms/question/q7.tsx
@@ -2,7 +2,7 @@ import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
-import { useSetRecoilState, useRecoilState, useRecoilValue } from 'recoil';
+import { useSetRecoilState, useRecoilValue } from 'recoil';
 import {
   healthInsAfterRetirementState,
   familyState,
@@ -15,15 +15,15 @@ interface formInput {
 }
 
 const Q7 = () => {
+  const [tab, setTab] = useState(1);
   const setHealthInsAfterRetirement = useSetRecoilState(
     healthInsAfterRetirementState,
   );
-  const [storedFamilyState] = useRecoilValue(familyState);
-  const [storedHealthInsLastTwoMonthState] = useRecoilValue(
+  const storedFamilyState = useRecoilValue(familyState);
+  const storedHealthInsLastTwoMonthState = useRecoilValue(
     healthInsLastTwoMonthState,
   );
   const [insuranceTypes, setInsuranceTypes] = useState(['']);
-  const [tab, setTab] = useState(1);
 
   useEffect(() => {
     const newInsuranceTypes = ['国民健康保険'];
