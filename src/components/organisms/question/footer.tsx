a74f605db6ec1c52c37b58077798d04d8754cb79
pachikuriii <stakahashi0728@gmail.com>
2023-02-03 23:50:46 +0900

    コンポーネントにおけるcssの設定を削除

--- src/components/organisms/question/footer.tsx
+++ src/components/organisms/question/footer.tsx
@@ -1,7 +1,6 @@
-import styles from './Footer.module.css';
 export default function Footer() {
   return (
-    <footer className={styles.box}>
+    <footer>
       <p>©︎ Saya</p>
     </footer>
   );
