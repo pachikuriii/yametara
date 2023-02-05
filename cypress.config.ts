510799bb1a5283be70956a33c4d3ba8ef0a6385a
pachikuriii <stakahashi0728@gmail.com>
2023-02-05 21:07:52 +0900

    プロジェクトIDを追加

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,7 +1,8 @@
-import { defineConfig } from "cypress";
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
+    projectId: "yametara",
     baseUrl: 'http://localhost:3000',
     setupNodeEvents(on, config) {
       // implement node event listeners here
@@ -10,8 +11,8 @@ export default defineConfig({
 
   component: {
     devServer: {
-      framework: "next",
-      bundler: "webpack",
+      framework: 'next',
+      bundler: 'webpack',
     },
   },
 });
