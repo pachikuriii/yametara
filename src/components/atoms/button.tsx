39a2733b55a69da7c14d49e856a991661c0e10d2
pachikuriii <stakahashi0728@gmail.com>
2023-02-06 17:15:07 +0900

    ボタンのデザインを変更

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -1,15 +1,16 @@
-import { motion } from 'framer-motion';
-import { ReactNode } from 'react';
-interface Props {
+import { ButtonHTMLAttributes, ReactNode } from 'react';
+
+type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
   onClick?: () => void;
   children: ReactNode;
-}
+};
 
-const Button = ({ onClick, children }: Props) => {
+const Button = ({ children, onClick, ...props }: Props) => {
   return (
     <button
-      className='btn btn-outline text-accent bg-primary rounded-full border-secondary no-animation hover:bg-secondary-focus shadow-md'
+      className='h-24 w-24 btn btn-outline text-accent bg-white rounded-2xl border-2  border-primary no-animation hover:bg-primary-focus  hover:border-primary-focus font-extrabold shadow-select'
       onClick={onClick}
+      {...props}
     >
       {children}
     </button>
