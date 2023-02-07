b06456eb5ec70327c11f0d0f0f6d84d765c56973
pachikuriii <stakahashi0728@gmail.com>
2023-02-07 21:13:09 +0900

    ルーティングの機能をページャーボタンのコンポーネントへ移動

--- src/components/molecules/buttons-pager.tsx
+++ src/components/molecules/buttons-pager.tsx
@@ -1,17 +1,40 @@
+import { useRouter } from 'next/router';
+import { BaseSyntheticEvent } from 'react';
 import GuideButton from '../atoms/guide-button';
+import { usePrevPage, useNextPage } from 'src/hooks/use-get-page';
 
 type Props = {
-  handleSubmit: () => void;
-  goBackPage: () => void;
+  handleSubmit(
+    e?: BaseSyntheticEvent<object, any, any> | undefined,
+  ): Promise<void>;
 };
 
-const PagerButtons = ({ handleSubmit, goBackPage }: Props) => {
+const PagerButtons = ({ handleSubmit }: Props) => {
+  const prevPage = usePrevPage();
+  const nextPage = useNextPage();
+  const router = useRouter();
+
+  const goPrevPage = () => {
+    router.push(prevPage);
+  };
+  const goNextPage = () => {
+    router.push(nextPage);
+  };
+
   return (
     <div className='flex w-full justify-center space-x-56'>
-      <GuideButton type='button' onClick={goBackPage}>
+      <GuideButton type='button' onClick={goPrevPage}>
         戻る
       </GuideButton>
-      <GuideButton onClick={handleSubmit}>次へ</GuideButton>
+      <GuideButton
+        type='button'
+        onClick={() => {
+          handleSubmit();
+          goNextPage();
+        }}
+      >
+        次へ
+      </GuideButton>
     </div>
   );
 };
