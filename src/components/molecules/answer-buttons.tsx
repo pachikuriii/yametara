6791b4f09739853776a53dbdbf8786c7cbd4116a
pachikuriii <stakahashi0728@gmail.com>
2023-02-07 18:39:51 +0900

    質問ページ選択用のボタンがコンポーネント内で生成されるように

---
+++ src/components/molecules/answer-buttons.tsx
@@ -0,0 +1,40 @@
+import { UseFormSetValue } from 'react-hook-form';
+import AnswerSelectButton from '../atoms/answer-button';
+import { formInput } from 'src/types/type';
+type Props = {
+  labels: Array<string>;
+  setValue: UseFormSetValue<formInput>;
+  property:
+    | 'retirementDate'
+    | 'retirementReason'
+    | 're_employment'
+    | 'age'
+    | 'postcode'
+    | 'family'
+    | 'emp_ins_total'
+    | 'emp_ins_last_two_years'
+    | 'health_ins_last_two_month'
+    | 'health_ins_after_retirement'
+    | 'tax';
+};
+
+const AnswerSelectButtons = ({ labels, setValue, property }: Props) => {
+  return (
+    <div className='space-x-4'>
+      {labels.map((value, index) => {
+        index += 1;
+        return (
+          <AnswerSelectButton
+            type='button'
+            key={index}
+            onClick={() => setValue(property, index)}
+          >
+            {value}
+          </AnswerSelectButton>
+        );
+      })}
+    </div>
+  );
+};
+
+export default AnswerSelectButtons;
