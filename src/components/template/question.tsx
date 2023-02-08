1f3a3675b18c127faf7b0ea1b0d270aaebf9e7db
pachikuriii <stakahashi0728@gmail.com>
2023-02-08 22:32:31 +0900

    質問のテンプレートに遷移前のページを見てモーションを制御する記述を追加

--- src/components/template/question.tsx
+++ src/components/template/question.tsx
@@ -1,19 +1,52 @@
-import { ReactNode } from 'react';
+import { useRouter } from 'next/router';
+import { ReactNode, useContext } from 'react';
+import BackMotion from '../atoms/back-motion';
 import Card from '../atoms/card';
+import FadeMotion from '../atoms/fade-motion';
 import Motion from '../atoms/motion';
 import AnswerProgressBar from '../atoms/progress-bar';
+import { HistoryContext } from 'src/hooks/history-context';
 interface Props {
   children: ReactNode;
 }
 
 const Question = ({ children }: Props) => {
+  const history = useContext(HistoryContext);
+  const router = useRouter();
+  const currentPageNumber = Number(
+    router.pathname.replace(/^\/questions\//, ''),
+  );
+  const previousPageNumber = Number(history[1].replace(/^\/questions\//, ''));
+  const isNext = currentPageNumber > previousPageNumber;
+  const isBack = currentPageNumber < previousPageNumber;
+  const fromTop = history[1] === '/';
+  const fromResult = history[1] === '/result';
+
   return (
     <>
       <div className='flex-grow'>
         <AnswerProgressBar></AnswerProgressBar>
-        <Motion>
-          <Card>{children}</Card>
-        </Motion>
+        {(() => {
+          if (isNext || fromTop) {
+            return (
+              <Motion>
+                <Card>{children}</Card>
+              </Motion>
+            );
+          } else if (isBack || fromResult) {
+            return (
+              <BackMotion>
+                <Card>{children}</Card>
+              </BackMotion>
+            );
+          } else {
+            return (
+              <FadeMotion>
+                <Card>{children}</Card>
+              </FadeMotion>
+            );
+          }
+        })()}
       </div>
     </>
   );
