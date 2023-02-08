ff4c4b7fee6877952d8d0a7515b8ac219f4c833b
pachikuriii <stakahashi0728@gmail.com>
2023-02-08 12:45:18 +0900

    ボタンの配置幅を変更

--- src/components/molecules/buttons-pager.tsx
+++ src/components/molecules/buttons-pager.tsx
@@ -14,18 +14,23 @@ const PagerButtons = ({ handleSubmit }: Props) => {
   const router = useRouter();
 
   return (
-    <div className='flex w-full justify-center space-x-56'>
-      <GuideButton type='button' onClick={() => router.push(prevPage)}>
-        戻る
-      </GuideButton>
-      <GuideButton
-        type='button'
-        onClick={() => {
-          handleSubmit();
-        }}
-      >
-        次へ
-      </GuideButton>
+    <div className='flex w-full justify-center'>
+      <div className='mr-auto'>
+        <GuideButton type='button' onClick={() => router.push(prevPage)}>
+          戻る
+        </GuideButton>
+      </div>
+
+      <div className='ml-auto'>
+        <GuideButton
+          type='button'
+          onClick={() => {
+            handleSubmit();
+          }}
+        >
+          次へ
+        </GuideButton>
+      </div>
     </div>
   );
 };
