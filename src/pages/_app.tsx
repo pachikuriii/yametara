b04894d9ba936ffcc09db266447c5c6baa324b1b
pachikuriii <stakahashi0728@gmail.com>
2023-02-08 22:31:26 +0900

    遷移前、遷移後のページを取得するためのファイル設定

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,21 +2,34 @@ import '../styles/globals.css';
 import { Noto_Sans_JP } from '@next/font/google';
 import { AnimatePresence } from 'framer-motion';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useState, useEffect } from 'react';
 import { RecoilRoot } from 'recoil';
+import { HistoryContext } from 'src/hooks/history-context';
 
 const NotoSansJP = Noto_Sans_JP({
   weight: '400',
   subsets: ['japanese'],
 });
 
-export default function App({ Component, pageProps, router }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+  const [history, setHistory] = useState([router.asPath, '']);
+
+  useEffect(() => {
+    setHistory([router.asPath, history[0]]);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [router.asPath]);
+
   return (
-    <RecoilRoot>
-      <main className={NotoSansJP.className}>
-        <AnimatePresence mode='wait'>
-          <Component key={router.asPath} {...pageProps} />
-        </AnimatePresence>
-      </main>
-    </RecoilRoot>
+    <HistoryContext.Provider value={history}>
+      <RecoilRoot>
+        <main className={NotoSansJP.className}>
+          <AnimatePresence mode='wait'>
+            <Component key={router.asPath} {...pageProps} />
+          </AnimatePresence>
+        </main>
+      </RecoilRoot>
+    </HistoryContext.Provider>
   );
 }
