bf2881104a747864c4b4e64c336af8087c5df7c1
pachikuriii <stakahashi0728@gmail.com>
2023-02-08 13:19:52 +0900

    カード内のテキストを中央揃え

--- src/components/atoms/card.tsx
+++ src/components/atoms/card.tsx
@@ -6,7 +6,9 @@ interface Props {
 const Card = ({ children }: Props) => {
   return (
     <div className='card max-sm:w-full md:w-96 bg-base-100 shadow-basic h-max text-base-content'>
-      <div className='card-body py-6 px-4 text-gray-800'>{children}</div>
+      <div className='card-body py-6 px-4 text-gray-800 text-center'>
+        {children}
+      </div>
     </div>
   );
 };
