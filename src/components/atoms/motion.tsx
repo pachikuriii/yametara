46a95491d98a2263932e67b769258470fd0a6f80
pachikuriii <stakahashi0728@gmail.com>
2023-02-09 19:04:09 +0900

    グローバルなステートを確認してアニメーションが制御されるように実装

--- src/components/atoms/motion.tsx
+++ src/components/atoms/motion.tsx
@@ -1,16 +1,48 @@
 import { motion } from 'framer-motion';
-import { ReactNode } from 'react';
+import { ReactNode, useEffect } from 'react';
+import { useRecoilState } from 'recoil';
+import {
+  isNextButtonClicked,
+  isBackButtonClicked,
+  nextMotionState,
+  backMotionState,
+} from 'src/motion-controller';
 
 interface Props {
   children: ReactNode;
 }
 
 const Motion = ({ children }: Props) => {
+  const [storedNextButtonClicked] = useRecoilState(isNextButtonClicked);
+  const [storedBackButtonClicked] = useRecoilState(isBackButtonClicked);
+  const [storedInitialMotion, setStoredInitialMotion] =
+    useRecoilState(nextMotionState);
+  const [storedExitMotion, setStoredExitMotion] =
+    useRecoilState(backMotionState);
+
+  useEffect(() => {
+    if (storedNextButtonClicked && !storedBackButtonClicked) {
+      setStoredInitialMotion('100%');
+      setStoredExitMotion('-100%');
+    } else if (!storedNextButtonClicked && storedBackButtonClicked) {
+      setStoredInitialMotion('-100%');
+      setStoredExitMotion('100%');
+    } else {
+      setStoredInitialMotion('0%');
+      setStoredExitMotion('0%');
+    }
+  }, [
+    storedNextButtonClicked,
+    storedBackButtonClicked,
+    setStoredInitialMotion,
+    setStoredExitMotion,
+  ]);
+
   return (
     <motion.div
-      initial={{ x: '100%' }}
+      initial={{ x: `${storedInitialMotion}` }}
       animate={{ x: 0 }}
-      exit={{ x: '-100%' }}
+      exit={{ x: `${storedExitMotion}` }}
       transition={{ duration: 0.3 }}
       className='flex justify-center mt-10 w-11/12 mx-auto'
     >
