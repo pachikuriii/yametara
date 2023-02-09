2e375edb42820b8dc232d3fda918ca64981907b3
pachikuriii <stakahashi0728@gmail.com>
2023-02-09 18:59:32 +0900

    元の状態に戻した

--- src/components/template/question.tsx
+++ src/components/template/question.tsx
@@ -1,54 +1,20 @@
-import { useRouter } from 'next/router';
-import { ReactNode, useContext, useState, useMemo } from 'react';
-import BackMotion from '../atoms/back-motion';
+import { ReactNode } from 'react';
 import Card from '../atoms/card';
-import FadeMotion from '../atoms/fade-motion';
 import Motion from '../atoms/motion';
 import AnswerProgressBar from '../atoms/progress-bar';
-import { HistoryContext } from 'src/hooks/history-context';
+
 interface Props {
   children: ReactNode;
 }
+
 const Question = ({ children }: Props) => {
-  const history = useContext(HistoryContext);
-  const router = useRouter();
-  const currentPageNumber = Number(
-    router.pathname.replace(/^\/questions\//, ''),
-  );
-  const previousPageNumber = Number(history[1].replace(/^\/questions\//, ''));
-  const isNext = currentPageNumber > previousPageNumber;
-  const isBack = currentPageNumber < previousPageNumber;
-  const fromTop = history[1] === '/';
-  const fromResult = history[1] === '/result';
-  const [state, setState] = useState(<></>);
-
-  useMemo(() => {
-    if (isNext || fromTop) {
-      setState(
-        <Motion>
-          <Card>{children}</Card>
-        </Motion>,
-      );
-    } else if (isBack || fromResult) {
-      setState(
-        <BackMotion>
-          <Card>{children}</Card>
-        </BackMotion>,
-      );
-    } else {
-      setState(
-        <FadeMotion>
-          <Card>{children}</Card>
-        </FadeMotion>,
-      );
-    }
-  }, [children, fromResult, fromTop, isBack, isNext]);
-
   return (
     <>
       <div className='flex-grow'>
         <AnswerProgressBar></AnswerProgressBar>
-        {state}
+        <Motion>
+          <Card>{children}</Card>
+        </Motion>
       </div>
     </>
   );
