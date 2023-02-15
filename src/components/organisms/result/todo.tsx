b700dd75bb1d3e00b6fb7d509df7e3ff844b1384
pachikuriii <stakahashi0728@gmail.com>
2023-02-15 17:34:44 +0900

    ページに含めていた文言をコンポーネントに移動した

--- src/components/organisms/result/todo.tsx
+++ src/components/organisms/result/todo.tsx
@@ -1,13 +1,17 @@
 import { useState, useEffect } from 'react';
 import { useRecoilState } from 'recoil';
+import dayjs from '../../../day-js';
 import {
   taxState,
   empInsLastTwoYearsState,
   retirementReasonState,
+  retirementDateState,
 } from '../../../session-stroage';
 import CheckedTodoPlate from '../../molecules/checked-todo-plate';
 
 const Todo = () => {
+  const [retirementDate] = useRecoilState(retirementDateState);
+  const [retirementDateToDisplay, setRetirementDateToDisplay] = useState('');
   const [storedTax] = useRecoilState(taxState);
   const [storedEmpInsLastTwoYears] = useRecoilState(empInsLastTwoYearsState);
   const [storedRetirementReason] = useRecoilState(retirementReasonState);
@@ -18,8 +22,14 @@ const Todo = () => {
   useEffect(() => {
     setTax(storedTax);
     setEmpInsLastTwoYears(storedEmpInsLastTwoYears);
+    setRetirementDateToDisplay(dayjs(retirementDate).format('YYYY年M月DD日'));
     setRetirementReason(storedRetirementReason);
-  }, [storedTax, storedEmpInsLastTwoYears, storedRetirementReason]);
+  }, [
+    storedTax,
+    storedEmpInsLastTwoYears,
+    storedRetirementReason,
+    retirementDate,
+  ]);
 
   // 雇用保険
   const RETIRED_WITH_UNINTENTIONAL_REASON_AND_INSURANCE =
@@ -33,23 +43,35 @@ const Todo = () => {
 
   return (
     <>
-      <div className='flex flex-col w-1/3 '>
-        <CheckedTodoPlate>健康保険</CheckedTodoPlate>
-        <CheckedTodoPlate>年金</CheckedTodoPlate>
-        <CheckedTodoPlate
-          className={
-            RETIRED_WITH_UNINTENTIONAL_REASON_AND_INSURANCE ||
-            RETIRED_WITH_CONPANY_REASON_AND_INSURANCE ||
-            RETIRED_WITH_OWN_REASON_AND_INSURANCE
-              ? ''
-              : ' hidden'
-          }
-        >
-          雇用保険
-        </CheckedTodoPlate>
-        <CheckedTodoPlate className={tax === 2 ? '' : ' hidden'}>
-          税金
-        </CheckedTodoPlate>
+      <div className='mx-auto flex flex-col jutify-center text-center'>
+        <p>
+          あなたが{retirementDateToDisplay}
+          に会社を辞めたら以下についての手続きが必要です。
+        </p>
+        <div className='mx-auto flex flex-col jutify-center h-[calc(100vh-8rem)] py-20 text-center'>
+          <div className='h-full w-screen flex justify-center items-center'>
+            <div>
+              <CheckedTodoPlate>健康保険</CheckedTodoPlate>
+              <CheckedTodoPlate>年金</CheckedTodoPlate>
+              <CheckedTodoPlate
+                additionalClassName={
+                  RETIRED_WITH_UNINTENTIONAL_REASON_AND_INSURANCE ||
+                  RETIRED_WITH_CONPANY_REASON_AND_INSURANCE ||
+                  RETIRED_WITH_OWN_REASON_AND_INSURANCE
+                    ? ''
+                    : ' hidden'
+                }
+              >
+                雇用保険
+              </CheckedTodoPlate>
+              <CheckedTodoPlate
+                additionalClassName={tax === 2 ? '' : ' hidden'}
+              >
+                税金
+              </CheckedTodoPlate>
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
