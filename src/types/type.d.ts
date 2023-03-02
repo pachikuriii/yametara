c550f14cad3113ce1325fa5eaf2d3791685ba1ae
pachikuriii <stakahashi0728@gmail.com>
2023-03-02 14:30:41 +0900

    フォームの入力値の型を変更

--- src/types/type.d.ts
+++ src/types/type.d.ts
@@ -1,13 +1,13 @@
 export interface formInput {
   retirementDate: string;
-  retirementReason: number;
-  re_employment: number;
-  age: number;
+  retirementReason: string;
+  re_employment: string;
+  age: string;
   postcode: string;
-  family: number;
-  emp_ins_total: number;
-  emp_ins_last_two_years: number;
-  health_ins_last_two_month: number;
-  health_ins_after_retirement: number;
-  tax: number;
+  family: string;
+  emp_ins_total: string;
+  emp_ins_last_two_years: string;
+  health_ins_last_two_month: string;
+  health_ins_after_retirement: string;
+  tax: string;
 }
