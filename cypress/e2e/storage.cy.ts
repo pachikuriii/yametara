56a42f63ffe027e3cb03b68755da7b5c0389b22c
pachikuriii <stakahashi0728@gmail.com>
2023-03-03 18:12:27 +0900

    Q2において回答内容がsessionStorageへ保存されることのテストを追加

--- cypress/e2e/storage.cy.ts
+++ cypress/e2e/storage.cy.ts
@@ -24,5 +24,17 @@ describe('sessionStorageへ値が保存されることのテスト', () => {
       cy.wrap(storage).its("retirement_reason").should('eq', 2)
      });
   })
+
+  it('Q2において回答内容がsessionStorageへ保存されること', () => {
+    sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-05-06" }));
+    cy.visit('/questions/2')
+    cy.get('#re-employment-form3').click()
+    cy.get('#next-page-button').click()
+    cy.window().then(win => {
+      const storage = JSON.parse(win.sessionStorage.getItem('yametara') || "[]");
+      cy.wrap(storage).its("re_employment").should('eq', 3)
+     });
+  })
+  
 })
 export {};
