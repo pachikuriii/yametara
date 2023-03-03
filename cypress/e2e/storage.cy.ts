fecefdb1a50940c1db96291c7368eef6680df663
pachikuriii <stakahashi0728@gmail.com>
2023-03-03 21:11:42 +0900

    Q7において回答内容がsessionStorageへ保存されることのテストを追記

--- cypress/e2e/storage.cy.ts
+++ cypress/e2e/storage.cy.ts
@@ -83,5 +83,16 @@ describe('sessionStorageへ値が保存されることのテスト', () => {
       cy.wrap(storage).its("health_ins_last_two_month").should('eq', 1)
      });
   })
+
+  it('Q7において回答内容がsessionStorageへ保存されること', () => {
+    sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-05-06", re_employment: 3, age: 1, post_code:'655-0873', family: 1, emp_ins_last_two_years: 2, emp_ins_total: 3, health_ins_last_two_month: 1  }));
+    cy.visit('/questions/7')
+    cy.get('#health-ins-after-retirement-form1').click()
+    cy.get('#next-page-button').click()
+    cy.window().then(win => {
+      const storage = JSON.parse(win.sessionStorage.getItem('yametara') || "[]");
+      cy.wrap(storage).its("health_ins_after_retirement").should('eq', 1)
+     });
+  })
 })
 export { };
