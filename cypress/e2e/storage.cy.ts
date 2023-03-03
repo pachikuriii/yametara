473c1adbf70311178e1ae5198e4c5c9bd8baa95d
pachikuriii <stakahashi0728@gmail.com>
2023-03-03 18:11:25 +0900

    フォームの補完もあわせてテストできるように入力値を変更

--- cypress/e2e/storage.cy.ts
+++ cypress/e2e/storage.cy.ts
@@ -7,22 +7,22 @@ describe('sessionStorageへ値が保存されることのテスト', () => {
     cy.window().then(win=> {
       const storage = JSON.parse(win.sessionStorage.getItem('yametara') || "[]");
       cy.wrap(storage).its("started").should('eq', true)
-    }); 
+    });
     cy.get('#next-page-button').click()
   })
 
   it('Q1において回答内容がsessionStorageへ保存されること', () => {
     sessionStorage.setItem('yametara', JSON.stringify({ started: true }));
     cy.visit('/questions/1')
-    cy.get('#retirement-date-form').type('2022-2-22')
+    cy.get('#retirement-date-form').type('2022-5-6')
     cy.get('#retirement-reason-form2').click()
     cy.get('#next-page-button').click()
     cy.window().then(win => {
       const storage = JSON.parse(win.sessionStorage.getItem('yametara') || "[]");
        
-      cy.wrap(storage).its("retirement_date").should('eq', '2022-02-22')
+      cy.wrap(storage).its("retirement_date").should('eq', '2022-05-06')
       cy.wrap(storage).its("retirement_reason").should('eq', 2)
-     }); 
+     });
   })
 })
 export {};
