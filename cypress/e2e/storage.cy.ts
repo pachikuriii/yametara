37d5a6b9dbfd5416afae0ca922331bafebd23a8c
pachikuriii <stakahashi0728@gmail.com>
2023-03-05 16:40:57 +0900

    初期値に退職事由の回答がもれていたので追加

--- cypress/e2e/storage.cy.ts
+++ cypress/e2e/storage.cy.ts
@@ -15,18 +15,18 @@ describe('sessionStorageへ値が保存されることのテスト', () => {
     sessionStorage.setItem('yametara', JSON.stringify({ started: true }));
     cy.visit('/questions/1')
     cy.get('#retirement-date-form').type('2022-5-6')
-    cy.get('#retirement-reason-form2').click()
+    cy.get('#retirement-reason-form1').click()
     cy.get('#next-page-button').click()
     cy.window().then(win => {
       const storage = JSON.parse(win.sessionStorage.getItem('yametara') || "[]");
        
       cy.wrap(storage).its("retirement_date").should('eq', '2022-05-06')
-      cy.wrap(storage).its("retirement_reason").should('eq', 2)
+      cy.wrap(storage).its("retirement_reason").should('eq', 1)
      });
   })
 
   it('Q2において回答内容がsessionStorageへ保存されること', () => {
-    sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-05-06" }));
+    sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-05-06", retirement_reason: 1  }));
     cy.visit('/questions/2')
     cy.get('#re-employment-form3').click()
     cy.get('#next-page-button').click()
@@ -37,7 +37,7 @@ describe('sessionStorageへ値が保存されることのテスト', () => {
   })
 
   it('Q3において回答内容がsessionStorageへ保存されること', () => {
-    sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-05-06", re_employment: 3 }));
+    sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-05-06", retirement_reason: 1, re_employment: 3 }));
     cy.visit('/questions/3')
     cy.get('#age-form1').click()
     cy.get('#postcode-form').type('6550873')
@@ -50,7 +50,7 @@ describe('sessionStorageへ値が保存されることのテスト', () => {
   })
 
   it('Q4において回答内容がsessionStorageへ保存されること', () => {
-    sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-05-06", re_employment: 3, age: 1, post_code:'655-0873' }));
+    sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-05-06", retirement_reason: 1, re_employment: 3, age: 1, post_code:'655-0873' }));
     cy.visit('/questions/4')
     cy.get('#family-form1').click()
     cy.get('#next-page-button').click()
@@ -61,7 +61,7 @@ describe('sessionStorageへ値が保存されることのテスト', () => {
   })
   
   it('Q5において回答内容がsessionStorageへ保存されること', () => {
-    sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-05-06", re_employment: 3, age: 1, post_code:'655-0873', family: 1 }));
+    sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-05-06", retirement_reason: 1, re_employment: 3, age: 1, post_code:'655-0873', family: 1 }));
     cy.visit('/questions/5')
     cy.get('#emp-ins-last-two-years-form2').click()
     cy.get('#emp-ins-total-form3').click()
@@ -74,7 +74,7 @@ describe('sessionStorageへ値が保存されることのテスト', () => {
   })
 
   it('Q6において回答内容がsessionStorageへ保存されること', () => {
-    sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-05-06", re_employment: 3, age: 1, post_code:'655-0873', family: 1, emp_ins_last_two_years: 2, emp_ins_total: 3  }));
+    sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-05-06", retirement_reason: 1, re_employment: 3, age: 1, post_code:'655-0873', family: 1, emp_ins_last_two_years: 2, emp_ins_total: 3  }));
     cy.visit('/questions/6')
     cy.get('#health-ins-last-two-month-form1').click()
     cy.get('#next-page-button').click()
@@ -85,7 +85,7 @@ describe('sessionStorageへ値が保存されることのテスト', () => {
   })
 
   it('Q7において回答内容がsessionStorageへ保存されること', () => {
-    sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-05-06", re_employment: 3, age: 1, post_code:'655-0873', family: 1, emp_ins_last_two_years: 2, emp_ins_total: 3, health_ins_last_two_month: 1  }));
+    sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-05-06", retirement_reason: 1, re_employment: 3, age: 1, post_code:'655-0873', family: 1, emp_ins_last_two_years: 2, emp_ins_total: 3, health_ins_last_two_month: 1  }));
     cy.visit('/questions/7')
     cy.get('#health-ins-after-retirement-form1').click()
     cy.get('#next-page-button').click()
@@ -96,7 +96,7 @@ describe('sessionStorageへ値が保存されることのテスト', () => {
   })
 
   it('Q8において回答内容がsessionStorageへ保存されること', () => {
-    sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-05-06", re_employment: 3, age: 1, post_code:'655-0873', family: 1, emp_ins_last_two_years: 2, emp_ins_total: 3, health_ins_last_two_month: 1, health_ins_after_retirement: 1  }));
+    sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-05-06", retirement_reason: 1, re_employment: 3, age: 1, post_code:'655-0873', family: 1, emp_ins_last_two_years: 2, emp_ins_total: 3, health_ins_last_two_month: 1, health_ins_after_retirement: 1  }));
     cy.visit('/questions/8')
     cy.get('#tax-form1').click()
     cy.get('#next-page-button').click()
