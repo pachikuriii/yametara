e5760aeb6d15f5910f9113d949b3faf5c5f02c71
pachikuriii <stakahashi0728@gmail.com>
2023-03-05 11:54:38 +0900

    フックの整理を出力のためのコードにも反映させた

--- src/components/organisms/result/data-input.tsx
+++ src/components/organisms/result/data-input.tsx
@@ -1,35 +1,51 @@
-import {
-  useSample,
-  useDisplayRetirementDate,
-  useDisplayPostcode,
-} from '../../../hooks/use-input-confirmation';
-
-import {
-  retirementReasonState,
-  reEmploymentState,
-  ageState,
-  familyState,
-  empInsLastTwoYearsState,
-  empInsTotalState,
-  healthInsLastTwoMonthState,
-  healthInsAfterRetirementState,
-  taxState,
-} from '../../../session-stroage';
+import { useEffect, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import dayjs from '../../../day-js';
+import { useDisplayGivenOption } from '../../../hooks/use-display-given-option';
+import * as storage from '../../../session-stroage';
 
 export default function DataInput() {
+  const [retirementReason] = useRecoilState(storage.retirementReasonState);
+  const [reEmployment] = useRecoilState(storage.reEmploymentState);
+  const [age] = useRecoilState(storage.ageState);
+  const [family] = useRecoilState(storage.familyState);
+  const [empInsLastTwoYears] = useRecoilState(storage.empInsLastTwoYearsState);
+  const [empInsTotal] = useRecoilState(storage.empInsTotalState);
+  const [healthInsLastTwoMonth] = useRecoilState(
+    storage.healthInsLastTwoMonthState,
+  );
+  const [healthInsAfterRetirement] = useRecoilState(
+    storage.healthInsAfterRetirementState,
+  );
+  const [tax] = useRecoilState(storage.taxState);
+  const [retirementDate] = useRecoilState(storage.retirementDateState);
+  const [postcode] = useRecoilState(storage.postcodeState);
+
+  const [displayedRetirementDate, setDisplayedRetirementDate] = useState('');
+  const [displayedPostcode, setDisplayedPostcode] = useState('');
+
+  useEffect(() => {
+    setDisplayedRetirementDate(dayjs(retirementDate).format('YYYY年M月D日'));
+    setDisplayedPostcode(postcode);
+  }, [retirementDate, postcode]);
+
   return (
     <div className='text-black'>
       <p>あなたの入力内容</p>
       <p>Q1</p>
-      <p>退職予定日:{useDisplayRetirementDate()}</p>
+      <p>退職予定日:{displayedRetirementDate}</p>
       <p>
         退職事由:
-        {useSample(retirementReasonState, ['自己都合', '会社都合', 'その他'])}
+        {useDisplayGivenOption(retirementReason, [
+          '自己都合',
+          '会社都合',
+          'その他',
+        ])}
       </p>
       <p>Q2</p>
       <p>
         再就職の予定:
-        {useSample(reEmploymentState, [
+        {useDisplayGivenOption(reEmployment, [
           '年内の再就職の予定あり',
           '年内の再就職の予定なし',
           '年内の再就職の予定は未定',
@@ -39,7 +55,7 @@ export default function DataInput() {
       <p>Q3</p>
       <p>
         年齢:
-        {useSample(ageState, [
+        {useDisplayGivenOption(age, [
           '30歳未満',
           '30歳以上35歳未満',
           '35歳以上45歳未満',
@@ -47,11 +63,11 @@ export default function DataInput() {
           '60歳以上65歳未満',
         ])}
       </p>
-      <p>郵便番号:{useDisplayPostcode()}</p>
+      <p>郵便番号:{displayedPostcode}</p>
       <p>Q4</p>
       <p>
         家族：
-        {useSample(familyState, [
+        {useDisplayGivenOption(family, [
           '社会保険の被保険者の家族がいる',
           '社会保険の被保険者の家族はいない',
         ])}
@@ -60,7 +76,7 @@ export default function DataInput() {
       <p>雇用保険</p>
       <p>
         過去2年:
-        {useSample(empInsLastTwoYearsState, [
+        {useDisplayGivenOption(empInsLastTwoYears, [
           '6ヶ月未満の加入実績あり',
           '6ヶ月以上1年未満の加入実績あり',
           '1年以上の加入実績あり',
@@ -68,7 +84,7 @@ export default function DataInput() {
       </p>
       <p>
         これまで:
-        {useSample(empInsTotalState, [
+        {useDisplayGivenOption(empInsTotal, [
           '1年未満の加入実績あり',
           '1年以上5年未満の加入実績あり',
           '5年以上10年未満の加入実績あり',
@@ -79,7 +95,7 @@ export default function DataInput() {
       <p>Q6</p>
       <p>
         健康保険:
-        {useSample(healthInsLastTwoMonthState, [
+        {useDisplayGivenOption(healthInsLastTwoMonth, [
           '2ヶ月未満の加入実績あり',
           '2ヶ月以上の加入実績あり',
         ])}
@@ -87,7 +103,7 @@ export default function DataInput() {
       <p>Q7</p>
       <p>
         退職後に加入を検討したい健康保険:
-        {useSample(healthInsAfterRetirementState, [
+        {useDisplayGivenOption(healthInsAfterRetirement, [
           '国民健康保険',
           '任意継続被保険者になる',
           '家族の扶養に入る',
@@ -96,7 +112,7 @@ export default function DataInput() {
       <p>Q8</p>
       <p>
         退職後の住民税の支払い方:
-        {useSample(taxState, [
+        {useDisplayGivenOption(tax, [
           '一括徴収',
           '普通徴収',
           '今年度の住民税の支払いはなし',
