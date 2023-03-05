295cdff4b165ddd51d3f2e8013925021d48c6970
pachikuriii <stakahashi0728@gmail.com>
2023-03-05 22:03:52 +0900

    各手続きの詳細のテンプレートを作成

---
+++ src/components/atoms/todo-detail.tsx
@@ -0,0 +1,32 @@
+import { ReactNode } from 'react';
+interface Props {
+  what: ReactNode;
+  where: ReactNode;
+  when: ReactNode;
+  prepare: ReactNode;
+}
+
+const TodoDetail = ({ what, where, when, prepare }: Props) => {
+  return (
+    <div className='text-center'>
+      <p className='text-xs border-b-4 border-accent border-dotted w-fit'>
+        なにを？
+      </p>
+      <div className='text-xl font-extrabold'>{what}</div>
+      <p className='text-xs border-b-4 border-accent border-dotted w-fit'>
+        どこで？
+      </p>
+      <div>{where}</div>
+      <p className='text-xs border-b-4 border-accent border-dotted w-fit'>
+        いつまでに？
+      </p>
+      <div>{when}</div>
+      <p className='text-xs border-b-4 border-accent border-dotted w-fit'>
+        用意するもの
+      </p>
+      <div>{prepare}</div>
+    </div>
+  );
+};
+
+export default TodoDetail;
