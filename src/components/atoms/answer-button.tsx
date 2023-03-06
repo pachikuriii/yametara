ef4ee62f878b06e8cffbefba65c491f2c0182ea6
pachikuriii <stakahashi0728@gmail.com>
2023-03-06 15:42:59 +0900

    ボタンのcssにhiddenが追加されないように変更

--- src/components/atoms/answer-button.tsx
+++ src/components/atoms/answer-button.tsx
@@ -4,17 +4,15 @@ type Props = ButtonHTMLAttributes<HTMLButtonElement> & {
   onClick?: () => void;
   children: ReactNode;
   id?: string;
-  display?: string;
 };
 
-const AnswerSelectButton = ({ children, onClick, id, display }: Props) => {
+const AnswerSelectButton = ({ children, onClick, id }: Props) => {
   return (
     <div
       id={id}
       onClick={onClick}
       className={
-        'h-24 w-24 btn btn-outline text-accent bg-white rounded-2xl border-2  border-primary no-animation hover:bg-primary-focus  hover:border-primary-focus font-extrabold shadow-select peer-checked:bg-primary-focus peer-checked:text-white ' +
-        display
+        'h-24 w-24 btn btn-outline text-accent bg-white rounded-2xl border-2  border-primary no-animation hover:bg-primary-focus  hover:border-primary-focus font-extrabold shadow-select peer-checked:bg-primary-focus peer-checked:text-white '
       }
     >
       {children}
