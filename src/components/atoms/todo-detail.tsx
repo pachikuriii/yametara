6668e6b5773362b3d5b9a1f960a7611f31cd82cb
pachikuriii <stakahashi0728@gmail.com>
2023-03-06 12:32:54 +0900

    タグをh5に

--- src/components/atoms/todo-detail.tsx
+++ src/components/atoms/todo-detail.tsx
@@ -12,7 +12,7 @@ const TodoDetail = ({ what, where, when, prepare }: Props) => {
       <p className='text-xs border-b-4 border-accent border-dotted w-fit'>
         なにを？
       </p>
-      <div className='text-xl font-extrabold'>{what}</div>
+      <h5 className='text-xl font-extrabold'>{what}</h5>
       <p className='text-xs border-b-4 border-accent border-dotted w-fit'>
         どこで？
       </p>
