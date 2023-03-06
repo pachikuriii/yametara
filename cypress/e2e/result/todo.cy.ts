12e321b95831d31634eb753b7a0798d6b2c9c9ae
pachikuriii <stakahashi0728@gmail.com>
2023-03-06 20:16:13 +0900

    結果ページ上部のプレートの表示についてテストを追加

--- cypress/e2e/result/todo.cy.ts
+++ cypress/e2e/result/todo.cy.ts
@@ -11,13 +11,13 @@ describe('結果ページ上部に表示される要素がシミュレーショ�
     cy.get('#todo-container').find('#todo-health-insurance-plate').should('have.text', '健康保険')
   })
 
-    it('年金のプレートが表示される', () => {
+  it('年金のプレートが表示される', () => {
     sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-01-06", retirement_reason: 1, re_employment: 3, age: 1, post_code:'655-0873', family: 2, emp_ins_last_two_years: 2, emp_ins_total: 3, health_ins_last_two_month: 1, health_ins_after_retirement: 1, tax: 2   }));
     cy.visit('/result')
     cy.get('#todo-container').find('#todo-pension-plate').should('have.text', '年金')
   })
 
-    it('自己都合の退職かつ雇用保険の被保険者期間が退職までの2年間で1年以上あれば雇用保険のプレートが表示される', () => {
+  it('自己都合の退職かつ雇用保険の被保険者期間が退職までの2年間で1年以上あれば雇用保険のプレートが表示される', () => {
     sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-01-06", retirement_reason: 1, re_employment: 3, age: 1, post_code:'655-0873', family: 2, emp_ins_last_two_years: 3, emp_ins_total: 3, health_ins_last_two_month: 1, health_ins_after_retirement: 1, tax: 2   }));
     cy.visit('/result')
     cy.get('#todo-container').find('#todo-employment-insurance-plate').should('have.text', '雇用保険')
@@ -29,10 +29,22 @@ describe('結果ページ上部に表示される要素がシミュレーショ�
     cy.get('#todo-container').find('#todo-employment-insurance-plate').should('not.exist')
   })
 
-   it('税金のプレートが表示される', () => {
+  it('再就職の予定がない場合は税金のプレートが表示される', () => {
     sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-01-06", retirement_reason: 1, re_employment: 3, age: 1, post_code:'655-0873', family: 2, emp_ins_last_two_years: 2, emp_ins_total: 3, health_ins_last_two_month: 1, health_ins_after_retirement: 1, tax: 1   }));
     cy.visit('/result')
     cy.get('#todo-container').find('#todo-tax-plate').should('have.text', '税金')
   })
+  
+  it('住民税を普通徴収で納める場合は税金のプレートが表示される', () => {
+    sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-01-06", retirement_reason: 2, re_employment: 3, age: 1, post_code:'655-0873', family: 2, emp_ins_last_two_years: 2, emp_ins_total: 3, health_ins_last_two_month: 1, health_ins_after_retirement: 1, tax: 2   }));
+    cy.visit('/result')
+    cy.get('#todo-container').find('#todo-tax-plate').should('have.text', '税金')
   })
+
+  it('再就職の予定があり、かつ住民税を普通徴収以外の方法で納める場合は税金のプレートが表示されない', () => {
+    sessionStorage.setItem('yametara', JSON.stringify({ started: true, retirement_date: "2022-01-06", retirement_reason: 2, re_employment: 1, age: 1, post_code:'655-0873', family: 2, emp_ins_last_two_years: 2, emp_ins_total: 3, health_ins_last_two_month: 1, health_ins_after_retirement: 1, tax: 3   }));
+    cy.visit('/result')
+    cy.get('#todo-container').find('#todo-tax-plate').should('not.exist')
+  })
+})
 export { };
