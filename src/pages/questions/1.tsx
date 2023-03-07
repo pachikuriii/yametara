7bbdad63d5ecd4d0120d190392c4defd40b1d6d7
pachikuriii <stakahashi0728@gmail.com>
2023-03-07 22:32:55 +0900

    q1の補足のモーダルの中身を作成

--- src/pages/questions/1.tsx
+++ src/pages/questions/1.tsx
@@ -31,7 +31,52 @@ export default function Page1() {
         </div>
 
         <Modal id='retirement-reason'>
-          <p className='py-4'>質問1のモーダル</p>
+          <div className='w-full bg-primary'>
+            <h2 className='text-white text-xl text-center py-2 font-extrabold'>
+              退職事由について
+            </h2>
+          </div>
+          <div className='px-6 py-6'>
+            <div className='pb-4'>
+              <div className='flex justify-center pb-2'>
+                <p className='px-4 py-2 text-accent bg-white rounded-2xl border-2  border-primary no-animation  font-extrabold'>
+                  自己都合
+                </p>
+              </div>
+              <p className='text-center'>
+                「会社都合」と「その他」のいずれにも該当しない退職の場合
+              </p>
+            </div>
+            <div className='pb-4'>
+              <div className='flex justify-center pb-2'>
+                <p className='px-4 py-2 text-accent bg-white rounded-2xl border-2  border-primary no-animation  font-extrabold'>
+                  会社都合
+                </p>
+              </div>
+              <p className='text-center'>倒産/解雇等などでの退職の場合</p>
+            </div>
+            <div>
+              <div className='flex justify-center pb-2'>
+                <p className='px-4 py-2 text-accent bg-white rounded-2xl border-2  border-primary no-animation  font-extrabold'>
+                  その他
+                </p>
+              </div>
+              <div className='text-center'>
+                <p>疾病/けが/介護/通勤困難など</p>
+                <p>正当な理由のある自己都合での退職の場合</p>
+                <p className='text-xs'>
+                  ※「正当な理由」の詳細は
+                  <a
+                    className='link'
+                    href='https://www.hellowork.mhlw.go.jp/insurance/insurance_range.html'
+                  >
+                    ハローワークHP
+                  </a>
+                  を参照
+                </p>
+              </div>
+            </div>
+          </div>
         </Modal>
 
         <Footer></Footer>
