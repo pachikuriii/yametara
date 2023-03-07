cc4b866127c15a01100c6ac032bad95c67ce32e6
pachikuriii <stakahashi0728@gmail.com>
2023-03-07 22:33:35 +0900

    q5の補足のモーダルを作成

--- src/pages/questions/5.tsx
+++ src/pages/questions/5.tsx
@@ -1,4 +1,5 @@
 import Head from 'next/head';
+import Image from 'next/image';
 import Footer from '../../components/organisms/question/footer';
 import Header from '../../components/organisms/question/header';
 import Q5 from '../../components/organisms/question/q5';
@@ -28,7 +29,54 @@ export default function Home() {
           </Motion>
         </div>
         <Modal id='how-to-count-emp-period'>
-          <p className='py-4'>質問5のモーダル</p>
+          <div className='w-full bg-primary'>
+            <h2 className='text-white text-xl text-center py-2 font-extrabold'>
+              被保険者期間の数え方について
+            </h2>
+          </div>
+          <div className='px-6 py-6'>
+            <p className='pb-4'>
+              以下のいずれかに当てはまる場合は、それまでのすべての雇用保険の被保険者期間はカウントに含めないでください。
+            </p>
+            <ul>
+              <div className='pb-4'>
+                <div className='flex justify-center pb-2'>
+                  <p className='text-accent'>
+                    ・雇用保険の被保険者でない状態が1年以上続いたことがある場合
+                  </p>
+                </div>
+                <Image
+                  src='/employment-insurance/not-count-over-year.svg'
+                  alt='logo'
+                  width='400'
+                  height='100'
+                  className='mx-auto pb-4'
+                />
+                <Image
+                  src='/employment-insurance/count.svg'
+                  alt='logo'
+                  width='400'
+                  height='100'
+                  className='mx-auto'
+                />
+              </div>
+
+              <div>
+                <div className='flex justify-center pb-2'>
+                  <p className='text-accent '>
+                    ・雇用保険の被保険者でなくなったのち、失業手当の受給を受けたことがある場合
+                  </p>
+                </div>
+                <Image
+                  src='/employment-insurance/not-count-paid.svg'
+                  alt='logo'
+                  width='400'
+                  height='100'
+                  className='mx-auto'
+                />
+              </div>
+            </ul>
+          </div>
         </Modal>
         <Footer></Footer>
       </main>
