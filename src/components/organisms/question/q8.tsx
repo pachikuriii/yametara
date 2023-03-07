a992e145cb0bb2478003c6bdc295f9a3f8b407b7
pachikuriii <stakahashi0728@gmail.com>
2023-03-07 22:29:54 +0900

    補足のタブを追加

--- src/components/organisms/question/q8.tsx
+++ src/components/organisms/question/q8.tsx
@@ -2,32 +2,31 @@ import dayjs from 'dayjs';
 import { useRouter } from 'next/router';
 import { useState, useEffect } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
+import { IconContext } from 'react-icons';
+import { RiMoneyCnyCircleFill } from 'react-icons/ri';
 import { useRecoilValue, useRecoilState } from 'recoil';
 import { taxState, retirementDateState } from '../../../session-stroage';
 import { formInput } from '../../../types/type';
-import Alert from '../../atoms/alert';
 import AnswerSelectButton from 'src/components/atoms/answer-button';
 import PagerButtons from 'src/components/molecules/buttons-pager';
 import { useNextPage } from 'src/hooks/use-get-page';
 
 export default function Q8() {
+  const [tab, setTab] = useState(1);
   const storedRetirementDate = useRecoilValue(retirementDateState);
   const [storedTax, setStoredTax] = useRecoilState(taxState);
   const [retiredOnBetweenJanAndMay, setretiredOnBetweenJanAndMay] =
     useState(false);
-
-  const searchOptions = (index: number) => {
-    if (!(index === 2 && retiredOnBetweenJanAndMay)) {
-      return true;
-    }
-  };
 
   useEffect(() => {
     const retirementMonth = dayjs(storedRetirementDate).month() + 1;
     if ([...Array(5)].map((_, i) => i + 1).includes(retirementMonth)) {
       setretiredOnBetweenJanAndMay(true);
     }
-  }, [storedRetirementDate, retiredOnBetweenJanAndMay]);
+    if (storedTax) {
+      setTab(storedTax);
+    }
+  }, [storedRetirementDate, retiredOnBetweenJanAndMay, storedTax]);
 
   const {
     handleSubmit,
@@ -50,15 +49,8 @@ export default function Q8() {
 
   return (
     <div>
-      <p>{retiredOnBetweenJanAndMay}</p>
-      <div className={retiredOnBetweenJanAndMay === true ? '' : ' hidden'}>
-        <Alert>
-          1月から5月に退職する場合は基本的に会社を退職する際に、最後の給与または退職金から一括で残りの住民税を天引きしてもらうこととなります。
-        </Alert>
-      </div>
-
+      <h2 className='card-title'>今年度の残りの住民税の支払い方法</h2>
       <form>
-        <label htmlFor='tax'>今年度の残りの住民税の支払い方法</label>
         <div className='flex space-x-4 justify-center' id='answer-options'>
           {['一括徴収', '普通徴収', '今年度の住民税の支払いはない'].map(
             (value, index) => {
@@ -75,8 +67,11 @@ export default function Q8() {
                       className='form-check-input hidden peer'
                       id={`${index}`}
                     />
-                    {searchOptions(index) && (
-                      <AnswerSelectButton id={`tax-form${index}`}>
+                    {!(index === 2 && retiredOnBetweenJanAndMay) && (
+                      <AnswerSelectButton
+                        id={`tax-form${index}`}
+                        onClick={() => setTab(index)}
+                      >
                         {value}
                       </AnswerSelectButton>
                     )}
@@ -87,6 +82,72 @@ export default function Q8() {
           )}
         </div>
         {errors.tax && <p>{errors.tax.message}</p>}
+
+        <div className='relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded'>
+          <div className='px-4 py-5 flex-auto'>
+            <div className='tab-content tab-space'>
+              {tab === 1 && (
+                <div>
+                  <IconContext.Provider
+                    value={{
+                      className: 'global-class-name',
+                      style: {
+                        display: 'inline',
+                      },
+                      size: '1.3em',
+                    }}
+                  >
+                    <h3 className='text-md font-extrabold'>
+                      <RiMoneyCnyCircleFill /> 一括徴収
+                    </h3>
+                  </IconContext.Provider>
+                  <p className='text-xs pt-2'>未納分を一括で支払う方式</p>
+                  <div className='flex justify-center'>
+                    <ul className='text-xs text-left py-2 list-disc'>
+                      <li>
+                        会社が退職時の給与/退職金から翌年5月分まで住民税を天引きする
+                      </li>
+                      {retiredOnBetweenJanAndMay && (
+                        <li>
+                          1月から5月に退職する場合は退職時に最後の給与/退職金から未納分の住民税が一括で天引きされることが一般的
+                        </li>
+                      )}
+                    </ul>
+                  </div>
+                </div>
+              )}
+              {tab === 2 && (
+                <div>
+                  <IconContext.Provider
+                    value={{
+                      className: 'global-class-name',
+                      style: {
+                        display: 'inline',
+                      },
+                      size: '1.3em',
+                    }}
+                  >
+                    <h3 className='text-md font-extrabold'>
+                      <RiMoneyCnyCircleFill />
+                      普通徴収
+                    </h3>
+                  </IconContext.Provider>
+                  <p className='text-xs pt-2 text-left'>
+                    納税通知書で納税者自ら納税する方式
+                  </p>
+                  <div className='flex justify-center'>
+                    <ul className='text-xs text-left py-2 list-disc'>
+                      <li>
+                        自治体より送付される納税通知書（納付書）によって自分自身で年1回（一括）または年4回（分割）で納税する
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              )}
+            </div>
+          </div>
+        </div>
+
         <PagerButtons
           handleSubmit={handleSubmit(submitContent)}
           isValid={isValid}
