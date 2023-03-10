828fbfbe1f8a9572e485cc1c409b32fd4dc97e85
pachikuriii <stakahashi0728@gmail.com>
2023-03-10 15:16:08 +0900

    各質問へコンポーネントへの切り出しを反映

--- src/components/organisms/question/q7.tsx
+++ src/components/organisms/question/q7.tsx
@@ -1,7 +1,5 @@
 import { useState, useEffect } from 'react';
 import { useForm, SubmitHandler } from 'react-hook-form';
-import { IconContext } from 'react-icons';
-import { RiMoneyCnyCircleFill } from 'react-icons/ri';
 import { useRecoilState, useRecoilValue } from 'recoil';
 import {
   healthInsAfterRetirementState,
@@ -9,8 +7,12 @@ import {
   healthInsLastTwoMonthState,
 } from '../../../session-stroage';
 import { formInput } from '../../../types/type';
+import DependentInsurance from './tabs/health-insurance-after-retirement/dependent-insurance';
+import NationalInsurance from './tabs/health-insurance-after-retirement/national-insurance';
+import OptionalInsurance from './tabs/health-insurance-after-retirement/optional-insurance';
 import AnswerSelectButton from 'src/components/atoms/answer-button';
 import PagerButtons from 'src/components/molecules/pager-buttons';
+import TabTemplate from 'src/components/template/tab-template';
 
 const Q7 = () => {
   const [tab, setTab] = useState(1);
@@ -90,105 +92,16 @@ const Q7 = () => {
         )}
       </form>
 
-      <div className='relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded'>
-        <div className='px-4 py-5 flex-auto'>
-          <div className='tab-content tab-space'>
-            {tab === 1 && (
-              <div>
-                <IconContext.Provider
-                  value={{
-                    className: 'global-class-name',
-                    style: {
-                      display: 'inline',
-                    },
-                    size: '1.3em',
-                  }}
-                >
-                  <h3 className='text-md font-extrabold'>
-                    <RiMoneyCnyCircleFill /> 国民健康保険の特徴
-                  </h3>
-                </IconContext.Provider>
-                <p className='text-xs pt-2'>
-                  会社員や公務員以外の全ての住民を対象とした保険
-                </p>
-                <div className='flex justify-center'>
-                  <ul className='text-xs text-left py-2 list-disc'>
-                    <li>前年の所得や世帯人員数に応じて保険料が決まる</li>
-                    <li>保険料の減免制度が受けられる場合がある</li>
-                    <li>居住している市区町村により保険料額が異なる</li>
-                  </ul>
-                </div>
-              </div>
-            )}
-            {tab === 2 && (
-              <div>
-                <IconContext.Provider
-                  value={{
-                    className: 'global-class-name',
-                    style: {
-                      display: 'inline',
-                    },
-                    size: '1.3em',
-                  }}
-                >
-                  <h3 className='text-md font-extrabold'>
-                    <RiMoneyCnyCircleFill />
-                    任意継続制度の特徴
-                  </h3>
-                </IconContext.Provider>
-                <p className='text-xs pt-2 text-left'>
-                  一定条件のもとで個人の希望により、それまで加入していた健康保険に個人でさらに最長2年間継続して加入できる制度
-                </p>
-                <div className='flex justify-center'>
-                  <ul className='text-xs text-left py-2 list-disc'>
-                    <li>
-                      在職時は事業主と折半していた保険料を全額自己負担する形となるため原則、退職時の給与から天引きされる保険料の倍額（
-                      ≒
-                      退職時の標準報酬月額×継続しようとする健康保険が定める料率）が保険料
-                    </li>
-                    <li>扶養家族の保険料はかからない</li>
-                    <li>継続期間中に保険料は原則変わらない</li>
-                  </ul>
-                </div>
-              </div>
-            )}
-            {tab === 3 && (
-              <div>
-                <IconContext.Provider
-                  value={{
-                    className: 'global-class-name',
-                    style: {
-                      display: 'inline',
-                    },
-                    size: '1.3em',
-                  }}
-                >
-                  <h3 className='text-md font-extrabold'>
-                    <RiMoneyCnyCircleFill />
-                    家族の健康保険の特徴
-                  </h3>
-                </IconContext.Provider>
-                <p className='text-xs pt-2 text-left'>
-                  被保険者である家族が加入している健康保険へ被扶養者として加入することで同一の保険制度を利用できる制度
-                </p>
-                <div className='flex justify-center'>
-                  <ul className='text-xs text-left py-2 list-disc'>
-                    <li>
-                      被扶養者として認定されるためには、家族の範囲と収入について一定の条件を満たしている必要がある
-                    </li>
-                    <li>被扶養者の保険料の負担はなし</li>
-                  </ul>
-                </div>
-              </div>
-            )}
+      <TabTemplate>
+        {tab === 1 && <NationalInsurance />}
+        {tab === 2 && <OptionalInsurance />}
+        {tab === 3 && <DependentInsurance />}
+      </TabTemplate>
 
-            <PagerButtons
-              handleSubmit={handleSubmit(submitContent)}
-              isValid={isValid}
-            ></PagerButtons>
-          </div>
-        </div>
-      </div>
+      <PagerButtons
+        handleSubmit={handleSubmit(submitContent)}
+        isValid={isValid}
+      ></PagerButtons>
     </div>
   );
 };
