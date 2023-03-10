7c686755164e6d79500bc0bbb45a58ce9272ddba
pachikuriii <stakahashi0728@gmail.com>
2023-03-10 15:14:27 +0900

    tabのコンポーネント作成

---
+++ src/components/atoms/tab.tsx
@@ -0,0 +1,31 @@
+import { ReactNode } from 'react';
+import { IconContext } from 'react-icons';
+import { RiMoneyCnyCircleFill } from 'react-icons/ri';
+interface Props {
+  title: string;
+  explanation: string;
+  children: ReactNode;
+}
+
+const Tab = ({ title, explanation, children }: Props) => {
+  return (
+    <div>
+      <IconContext.Provider
+        value={{
+          className: 'global-class-name',
+          style: {
+            display: 'inline',
+          },
+          size: '1.3em',
+        }}
+      >
+        <h3 className='text-md font-extrabold'>
+          <RiMoneyCnyCircleFill /> {title}
+        </h3>
+      </IconContext.Provider>
+      <p className='text-xs pt-2'>{explanation}</p>
+      <div className='flex justify-center'>{children}</div>
+    </div>
+  );
+};
+export default Tab;
