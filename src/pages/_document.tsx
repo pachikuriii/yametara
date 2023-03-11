922be284dc703bb984dc0d289614aae5fad8e57f
pachikuriii <stakahashi0728@gmail.com>
2023-03-12 00:30:20 +0900

    favicon設定追加

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,7 +4,32 @@ export default function Document() {
   return (
     <Html lang='ja'>
       <Head prefix='og:http://ogp.me/ns#'>
-        <link rel='icon' href='/favicon.ico' />
+        <link
+          rel='apple-touch-icon'
+          sizes='76x76'
+          href='/favicon/apple-touch-icon.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='32x32'
+          href='/favicon/favicon-32x32.png'
+        />
+        <link
+          rel='icon'
+          type='image/png'
+          sizes='16x16'
+          href='/favicon/favicon-16x16.png'
+        />
+        <link rel='manifest' href='/public/favicon/site.webmanifest' />
+        <link
+          rel='mask-icon'
+          href='/public/favicon/safari-pinned-tab.svg'
+          color='#34d399'
+        />
+        <meta name='msapplication-TileColor' content='#da532c' />
+        <meta name='theme-color' content='#ffffff'></meta>
+
         <meta
           property='og:title'
           content='yametara｜退職後の社会保険や税金の手続き内容をシミュレーション'
