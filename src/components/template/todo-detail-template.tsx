aa2eb2137bf65b0e7100726c29c3ed4a9325e341
pachikuriii <stakahashi0728@gmail.com>
2023-03-11 20:26:23 +0900

    結果ページにスタイリング追加

--- src/components/template/todo-detail-template.tsx
+++ src/components/template/todo-detail-template.tsx
@@ -9,20 +9,29 @@ interface Props {
 
 const TodoDetailTemplate = ({ what, where, when, prepare, id }: Props) => {
   return (
-    <div className='text-center' id={id}>
-      <p className='text-xs border-b-4 border-accent border-dotted w-fit'>
-        なにを？
-      </p>
-      <h5 className='text-xl font-extrabold'>{what}</h5>
-      <p className='text-xs border-b-4 border-accent border-dotted w-fit'>
-        どこで？
-      </p>
-      <div>{where}</div>
-      <p className='text-xs border-b-4 border-accent border-dotted w-fit'>
-        いつまでに？
-      </p>
-      <div>{when}</div>
-      <p className='text-xs border-b-4 border-accent border-dotted w-fit'>
+    <div className='text-center pb-10' id={id}>
+      <div className='pb-2'>
+        <p className='mb-1 text-xs border-b-4 border-accent border-dotted w-fit'>
+          なにを？
+        </p>
+        <h5 className='text-xl font-extrabold'>{what}</h5>
+      </div>
+
+      <div className='pb-2'>
+        <p className='mb-1 text-xs border-b-4 border-accent border-dotted w-fit'>
+          どこで？
+        </p>
+        <div>{where}</div>
+      </div>
+
+      <div className='pb-2'>
+        <p className='mb-1 text-xs border-b-4 border-accent border-dotted w-fit'>
+          いつまでに？
+        </p>
+        <div>{when}</div>
+      </div>
+
+      <p className='mb-2 text-xs border-b-4 border-accent border-dotted w-fit'>
         用意するもの
       </p>
       <div>{prepare}</div>
