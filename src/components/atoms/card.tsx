93d945d8017e1a82c25ce641ff08441f38dc92d8
pachikuriii <stakahashi0728@gmail.com>
2023-03-11 13:02:03 +0900

    最大幅を指定

--- src/components/atoms/card.tsx
+++ src/components/atoms/card.tsx
@@ -6,7 +6,7 @@ interface Props {
 const Card = ({ children }: Props) => {
   return (
     <div className='card w-full text-gray-800 text-center bg-white shadow-basic'>
-      <div className='py-4 px-4'>{children}</div>
+      <div className='py-4 px-4 max-w-lg mx-auto'>{children}</div>
     </div>
   );
 };
