e0745e12395752a7aafcf161d5c651d64133a398
pachikuriii <stakahashi0728@gmail.com>
2023-04-17 22:02:48 +0900

    パーツをコンポーネントに切り出した

---
+++ src/components/molecules/answer-select-buttons.tsx
@@ -0,0 +1,48 @@
+import { FieldError } from 'react-hook-form';
+import AnswerSelectButton from '../atoms/answer-button';
+import Error from '../atoms/error';
+interface Props {
+  choices: string[];
+  register: any;
+  name: string;
+  errors: FieldError | undefined;
+  idPrefix: string;
+}
+const AnswerSelectButtons = ({
+  choices,
+  register,
+  name,
+  errors,
+  idPrefix,
+}: Props) => {
+  return (
+    <div>
+      <div className='flex space-x-2 justify-center pb-2'>
+        {choices.map((value, index) => {
+          index += 1;
+          return (
+            <div key={index}>
+              <label htmlFor={`${index}`}>
+                <input
+                  {...register(name, {
+                    required: '選択してください',
+                  })}
+                  type='radio'
+                  value={index}
+                  className='form-check-input hidden peer'
+                  id={`${index}`}
+                />
+                <AnswerSelectButton id={`${idPrefix}${index}`}>
+                  {value}
+                </AnswerSelectButton>
+              </label>
+            </div>
+          );
+        })}
+      </div>
+      {errors && <Error>{errors.message}</Error>}
+    </div>
+  );
+};
+
+export default AnswerSelectButtons;
---
+++ src/components/molecules/retirement-date-form.tsx
@@ -0,0 +1,46 @@
+import { Controller, Control, FieldErrorsImpl } from 'react-hook-form';
+import { NumberFormatBase } from 'react-number-format';
+import dayjs from '../../day-js';
+import Error from '../atoms/error';
+import { useRetirementDateInputHelper } from 'src/hooks/use-retirement-date-input-helper';
+import { formInput } from 'src/types/type';
+interface Props {
+  props: any;
+  storedRetirementDate: string;
+  control: Control<formInput>;
+  name: 'retirementDate';
+  errors: Partial<FieldErrorsImpl<formInput>>;
+}
+const RetirementDateForm = ({ props, control, name, errors }: Props) => {
+  const formattedValue = useRetirementDateInputHelper(props);
+  return (
+    <div>
+      <Controller
+        control={control}
+        rules={{
+          required: '入力してください',
+          pattern: {
+            value: /^(20[0-9]{2})-(0[1-9]|1[0-2])-(0[1-9]|[12][0-9]|3[01])+$/,
+            message: '有効な日付を入力してください',
+          },
+        }}
+        name={name}
+        render={({ field: { onChange, ref, ...rest } }) => (
+          <NumberFormatBase
+            id='retirement-date-form'
+            className='border-2 border-primary input input-bordered input-lg w-full text-center'
+            onChange={onChange}
+            placeholder={dayjs().format('YYYY-MM-DD')}
+            format={formattedValue}
+            {...rest}
+            {...props}
+            inputMode='numeric'
+          />
+        )}
+      />
+      {errors.retirementDate && <Error>{errors.retirementDate.message}</Error>}
+    </div>
+  );
+};
+
+export default RetirementDateForm;
