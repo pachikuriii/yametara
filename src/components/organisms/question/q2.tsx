cd099c6dce334687f223871a6ba37e877c45f80b
pachikuriii <stakahashi0728@gmail.com>
2023-04-19 14:21:54 +0900

    Q2のボタンをコンポーネントに切り出した

--- src/components/organisms/question/q2.tsx
+++ src/components/organisms/question/q2.tsx
@@ -2,9 +2,8 @@ import { useForm, SubmitHandler } from 'react-hook-form';
 import { useRecoilState } from 'recoil';
 import { reEmploymentState } from '../../../storage/session-stroage';
 import { formInput } from '../../../types/type';
-import AnswerSelectButton from 'src/components/atoms/answer-button';
-import Error from 'src/components/atoms/error';
 import QuestionTitle from 'src/components/atoms/question-title';
+import AnswerSelectButtons from 'src/components/molecules/answer-select-buttons';
 import PagerButtons from 'src/components/molecules/pager-buttons';
 export default function Q2() {
   const [storedReEmployment, setStoredReEmployment] =
@@ -30,30 +29,13 @@ export default function Q2() {
         <QuestionTitle>
           <label htmlFor='re_employment'>年内の再就職の予定</label>
         </QuestionTitle>
-        <div className='flex space-x-2 justify-center pb-2'>
-          {['あり', 'なし', '未定'].map((value, index) => {
-            index += 1;
-            return (
-              <div key={index}>
-                <label htmlFor={`${index}`}>
-                  <input
-                    {...register('re_employment', {
-                      required: '選択してください',
-                    })}
-                    type='radio'
-                    value={index}
-                    className='form-check-input hidden peer'
-                    id={`${index}`}
-                  />
-                  <AnswerSelectButton id={`re-employment-form${index}`}>
-                    {value}
-                  </AnswerSelectButton>
-                </label>
-              </div>
-            );
-          })}
-        </div>
-        {errors.re_employment && <Error>{errors.re_employment.message}</Error>}
+        <AnswerSelectButtons
+          choices={['あり', 'なし', '未定']}
+          name='re_employment'
+          register={register}
+          errors={errors.re_employment}
+          idPrefix={'re-employment-form'}
+        ></AnswerSelectButtons>
       </form>
 
       <PagerButtons
