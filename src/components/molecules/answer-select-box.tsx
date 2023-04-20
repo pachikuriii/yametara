842fc8043e6e75aee22c24a45f8d080675bb17bb
pachikuriii <stakahashi0728@gmail.com>
2023-04-20 15:24:39 +0900

    矢印を大きく

--- src/components/molecules/answer-select-box.tsx
+++ src/components/molecules/answer-select-box.tsx
@@ -30,7 +30,7 @@ const AnswerSelectBox = ({ options, register, name, errors }: Props) => {
         </select>
         <div className='pointer-events-none absolute inset-y-0 right-0 flex items-center px-2'>
           <svg
-            className='fill-current h-4 w-4'
+            className='fill-current h-8 w-8'
             xmlns='http://www.w3.org/2000/svg'
             viewBox='0 0 20 20'
           >
