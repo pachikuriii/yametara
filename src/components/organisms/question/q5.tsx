09dffd5d1894e6e70727dd46bab72e6aa145d5c1
pachikuriii <stakahashi0728@gmail.com>
2023-04-20 12:37:30 +0900

    コンポーネントを反映

--- src/components/organisms/question/q5.tsx
+++ src/components/organisms/question/q5.tsx
@@ -1,16 +1,13 @@
 import { useForm, SubmitHandler } from 'react-hook-form';
 import { useRecoilState } from 'recoil';
-import { Navigation } from 'swiper';
-import { Swiper, SwiperSlide } from 'swiper/react';
 import {
   empInsTotalState,
   empInsLastTwoYearsState,
 } from '../../../storage/session-stroage';
 import { formInput } from '../../../types/type';
 import Alert from 'src/components/atoms/alert';
-import AnswerSelectButton from 'src/components/atoms/answer-button';
-import Error from 'src/components/atoms/error';
 import QuestionTitle from 'src/components/atoms/question-title';
+import AnswerSelectBox from 'src/components/molecules/answer-select-box';
 import AnswerSelectButtons from 'src/components/molecules/answer-select-buttons';
 import PagerButtons from 'src/components/molecules/pager-buttons';
 
@@ -56,47 +53,18 @@ export default function Q5() {
         <label className='pb-1' htmlFor='emp_ins_total'>
           退職予定日までの通算では…
         </label>
-        <div className='flex space-x-4 justify-center'>
-          <Swiper
-            slidesPerView={1}
-            className='mySwiper'
-            navigation={true}
-            modules={[Navigation]}
-            centeredSlides={true}
-            initialSlide={storedEmpInsTotal ? Number(storedEmpInsTotal) - 1 : 0}
-            id='swiper'
-            style={{ width: '15rem' }}
-          >
-            {[
-              '1年未満',
-              '1年以上5年未満',
-              '5年以上10年未満',
-              '10年以上20年未満',
-              '20年以上',
-            ].map((value, index) => {
-              index += 1;
-              return (
-                <SwiperSlide key={index} style={{ height: '6.5rem' }}>
-                  <label htmlFor={`emp_ins_total${index}`}>
-                    <input
-                      {...register('emp_ins_total', {
-                        required: '選択してください',
-                      })}
-                      type='radio'
-                      value={index}
-                      className='form-check-input hidden peer'
-                      id={`emp_ins_total${index}`}
-                    />
-                    <AnswerSelectButton id={`emp-ins-total-form${index}`}>
-                      {value}
-                    </AnswerSelectButton>
-                  </label>
-                </SwiperSlide>
-              );
-            })}
-          </Swiper>
-        </div>
-        {errors.emp_ins_total && <Error>{errors.emp_ins_total.message}</Error>}
+        <AnswerSelectBox
+          options={[
+            '1年未満',
+            '1年以上5年未満',
+            '5年以上10年未満',
+            '10年以上20年未満',
+            '20年以上',
+          ]}
+          register={register}
+          name='emp_ins_total'
+          errors={errors.emp_ins_total}
+        ></AnswerSelectBox>
         <label htmlFor='how-to-count-emp-period' className='link'>
           <Alert>数え方について</Alert>
         </label>
