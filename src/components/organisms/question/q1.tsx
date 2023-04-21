d1a093b1be3d656afa230cd92ae9f708d58fdfe4
pachikuriii <stakahashi0728@gmail.com>
2023-04-21 11:33:59 +0900

    不要なプロパティ削除

--- src/components/organisms/question/q1.tsx
+++ src/components/organisms/question/q1.tsx
@@ -46,7 +46,6 @@ export default function Q1(props: any) {
             props={props}
             storedRetirementDate={storedRetirementDate}
             control={control}
-            name='retirementDate'
             errors={errors}
           ></RetirementDateForm>
         </div>
