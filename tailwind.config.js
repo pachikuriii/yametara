01a41f9e0042b5cab16418406d5079eb1c9393bc
pachikuriii <stakahashi0728@gmail.com>
2023-04-23 11:55:58 +0900

    シャドウ調整

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,14 +4,6 @@ module.exports = {
     './src/pages/**/*.{js,ts,jsx,tsx}',
     './src/components/**/*.{js,ts,jsx,tsx}',
   ],
-  theme: {
-    boxShadow: {
-      select: '0 9px 0px #34D399',
-      basic: '0 9px 0px #A7F3D0',
-      none: 'none',
-    },
-    extend: {},
-  },
   plugins: [require('daisyui')],
   daisyui: {
     themes: [
