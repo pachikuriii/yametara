dccb407d210aa8b5e11a809c47cb434bf478a5a2
pachikuriii <stakahashi0728@gmail.com>
2023-05-04 01:13:23 +0900

    ダイナミックインポートを適用

--- src/pages/questions/[id].tsx
+++ src/pages/questions/[id].tsx
@@ -1,28 +1,54 @@
+import dynamic from 'next/dynamic';
 import { useRouter } from 'next/router';
-import EmploymentInsuranceReference from 'src/components/organisms/question/modal/employment-insurance-reference';
-import RetirementReasonReference from 'src/components/organisms/question/modal/retirement-reason-reference';
-import Q1 from 'src/components/organisms/question/q1';
-import Q2 from 'src/components/organisms/question/q2';
-import Q3 from 'src/components/organisms/question/q3';
-import Q4 from 'src/components/organisms/question/q4';
-import Q5 from 'src/components/organisms/question/q5';
-import Q6 from 'src/components/organisms/question/q6';
-import Q7 from 'src/components/organisms/question/q7';
-import Q8 from 'src/components/organisms/question/q8';
-import ModalTemplate from 'src/components/template/modal-template';
+import Loading from 'src/components/atoms/loading';
 import QuestionTemplate from 'src/components/template/question-template';
+const Q1 = dynamic(() => import('src/components/organisms/question/q1'), {
+  loading: () => <Loading />,
+  ssr: false,
+});
+const Q2 = dynamic(() => import('src/components/organisms/question/q2'), {
+  loading: () => <p>Loading...</p>,
+});
+const Q3 = dynamic(() => import('src/components/organisms/question/q3'), {
+  loading: () => <p>Loading...</p>,
+});
+const Q4 = dynamic(() => import('src/components/organisms/question/q4'), {
+  loading: () => <p>Loading...</p>,
+});
+const Q5 = dynamic(() => import('src/components/organisms/question/q5'), {
+  loading: () => <p>Loading...</p>,
+});
+const Q6 = dynamic(() => import('src/components/organisms/question/q6'), {
+  loading: () => <p>Loading...</p>,
+});
+const Q7 = dynamic(() => import('src/components/organisms/question/q7'), {
+  loading: () => <p>Loading...</p>,
+});
+const Q8 = dynamic(() => import('src/components/organisms/question/q8'), {
+  loading: () => <p>Loading...</p>,
+});
+const EmploymentInsuranceReference = dynamic(
+  () =>
+    import(
+      'src/components/organisms/question/modal/employment-insurance-reference'
+    ),
+  { loading: () => <Loading />, ssr: false },
+);
+const RetirementReasonReference = dynamic(
+  () =>
+    import(
+      'src/components/organisms/question/modal/retirement-reason-reference'
+    ),
+  { loading: () => <Loading />, ssr: false },
+);
+const ModalTemplate = dynamic(
+  () => import('src/components/template/modal-template'),
+  { loading: () => <Loading />, ssr: false },
+);
 
-interface Props {
-  id: string | string[] | undefined;
-}
-
-interface pageTitle {
-  [key: string]: { title: string; todo: string };
-}
-
-const Content = ({ id }: Props) => {
+const Content = ({ id }: { id: string | string[] | undefined }) => {
   return (
-    <>
+    <div>
       {id === '1' && <Q1></Q1>}
       {id === '2' && <Q2></Q2>}
       {id === '3' && <Q3></Q3>}
@@ -32,11 +58,11 @@ const Content = ({ id }: Props) => {
       {id === '7' && <Q7></Q7>}
       {id === '8' && <Q8></Q8>}
       {!id && <h1>Loading...</h1>}
-    </>
+    </div>
   );
 };
 
-const pageTitle: pageTitle = {
+const pageTitle: { [key: string]: { title: string; todo: string } } = {
   '1': {
     title: '今回の退職について',
     todo: '【記入/選択してください】',
@@ -74,9 +100,6 @@ const pageTitle: pageTitle = {
 export default function Question() {
   const router = useRouter();
   const id = router.query.id;
-  if (id == null) {
-    return <h1>Loading...</h1>;
-  }
   return (
     <>
       <QuestionTemplate
