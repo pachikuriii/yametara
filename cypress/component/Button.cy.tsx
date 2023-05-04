33ce6440b7c8700aabe2ee508fc6d7c23c17cc9e
pachikuriii <stakahashi0728@gmail.com>
2023-05-05 01:05:32 +0900

    useRouterをmock

--- cypress/component/Button.cy.tsx
+++ cypress/component/Button.cy.tsx
@@ -1,4 +1,42 @@
-import Card from 'src/components/atoms/card';
-it('NextLink can be rendered', () => {
-  cy.mount(<Card>テスト</Card>);
+import { RouterContext } from 'next/dist/shared/lib/router-context';
+import { RecoilRoot } from 'recoil';
+import Q7 from 'src/components/organisms/question/q7';
+
+const createRouter = () => ({
+  route: '/',
+  pathname: '/',
+  query: {},
+  asPath: '/',
+  basePath: '',
+  back: cy.spy().as('back'),
+  beforePopState: cy.spy().as('beforePopState'),
+  forward: cy.spy().as('forward'),
+  prefetch: cy.stub().as('prefetch').resolves(),
+  push: cy.spy().as('push'),
+  reload: cy.spy().as('reload'),
+  replace: cy.spy().as('replace'),
+  events: {
+    emit: cy.spy().as('emit'),
+    off: cy.spy().as('off'),
+    on: cy.spy().as('on'),
+  },
+  isFallback: false,
+  isLocaleDomain: false,
+  isReady: true,
+  defaultLocale: 'en',
+  domainLocales: [],
+  isPreview: false,
+});
+
+describe('<CategoryPageList>', () => {
+  it('Product card click redirects to PDP', () => {
+    const router = createRouter();
+    cy.mount(
+      <RouterContext.Provider value={router}>
+        <RecoilRoot>
+          <Q7></Q7>
+        </RecoilRoot>
+      </RouterContext.Provider>,
+    );
+  });
 });
